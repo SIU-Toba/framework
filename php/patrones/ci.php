a3d213e7166763eb3aec4ed9747db6824d83138b
Sebastián Marconi <smarconi@siu.edu.ar>
2005-05-27 14:27:36 +0000

    Al patrón CI, no necesitan que le pasen la clase como parámetro.

--- php/patrones/ci.php
+++ php/patrones/ci.php
@@ -1,9 +1,14 @@
-<? 
-    $ci = $this->cargar_objeto($this->info["item_parametro_a"],0); 
-    if($ci > -1){ 
-		$this->objetos[$ci]->procesar_eventos();
-		$this->objetos[$ci]->generar_interface_grafica();	
-    }else{ 
-        echo ei_mensaje("No fue posible instanciar el CONTROLARDO de INTERFACE"); 
-    } 
+<?php
+	if ($this->info_objetos > 0) {
+		$objeto = current($this->info_objetos);
+	    $ci = $this->cargar_objeto($objeto['clase'],0); 
+	    if($ci > -1){ 
+			$this->objetos[$ci]->procesar_eventos();
+			$this->objetos[$ci]->generar_interface_grafica();	
+	    } else { 
+	        echo ei_mensaje("No fue posible instanciar el CONTROLARDO de INTERFACE"); 
+	    } 
+	} else { 
+		echo ei_mensaje("Necesita asociar un objeto CI al ítem."); 
+    }
 ?>
