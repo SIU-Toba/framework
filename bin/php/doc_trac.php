9f3cd7783aed2633845e9736a64842a299297288
Juan Bordón <jbordon@siu.edu.ar>
2005-05-30 19:43:09 +0000

    Documentacion de interfaces de las clases toba en trac: 0% terminado.

---
+++ bin/php/doc_trac.php
@@ -0,0 +1,20 @@
+<?
+	$dir_raiz = $argv[1];
+	$dir_clases = $dir_raiz . "/php/nucleo/browser/clases";
+	//Lista de archivos a documentar
+	$archivos[] = $dir_clases . "/objeto.php";
+	$archivos[] = $dir_clases . "/objeto_ci.php";
+	$archivos[] = $dir_clases . "/objeto_ci_me.php";
+	$archivos[] = $dir_clases . "/objeto_ci_me_tab.php";
+	$archivos[] = $dir_clases . "/objeto_ei_cuadro.php";
+	$archivos[] = $dir_clases . "/objeto_ei_filtro.php";
+	$archivos[] = $dir_clases . "/objeto_ei_formulario.php";
+	$archivos[] = $dir_clases . "/objeto_ei_formulario_ml.php";
+
+
+	foreach($archivos as $archivo)
+	{
+		//$php = file_get_contents($archivo);
+	}
+?>
+	
