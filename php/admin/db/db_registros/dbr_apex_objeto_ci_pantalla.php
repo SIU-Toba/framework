92845d3d37234d82ce3aaa5a807d9c7a330ce69a
Juan Bordón <jbordon@siu.edu.ar>
2005-07-23 08:41:15 +0000

    Reorganizacion del codigo de los editores. Se separaron los elementos (dbts, dbrs) que acceden el modelo de datos a una carpeta separada (es probable que sean utilizados desde otros lugares, EJ: El envio de elementos entre instancias podria construirse sobre la serializacion de db_tablas... )

---
+++ php/admin/db/db_registros/dbr_apex_objeto_ci_pantalla.php
@@ -0,0 +1,43 @@
+<?
+//Generacion: 23-07-2005 05:04:17
+//Fuente de datos: 'instancia'
+require_once('nucleo/persistencia/db_registros_s.php');
+
+class dbr_apex_objeto_ci_pantalla extends db_registros_s
+//db_registros especifico de la tabla 'apex_objeto_ci_pantalla'
+{
+	function __construct($fuente=null, $min_registros=0, $max_registros=0 )
+	{
+		$def['tabla']='apex_objeto_ci_pantalla';
+		$def['columna'][0]['nombre']='objeto_ci_proyecto';
+		$def['columna'][0]['pk']='1';
+		$def['columna'][0]['no_nulo']='1';
+		$def['columna'][1]['nombre']='objeto_ci';
+		$def['columna'][1]['pk']='1';
+		$def['columna'][1]['no_nulo']='1';
+		$def['columna'][2]['nombre']='pantalla';
+		$def['columna'][2]['pk']='1';
+		$def['columna'][2]['secuencia']='apex_obj_ci_pantalla_seq';
+		$def['columna'][3]['nombre']='identificador';
+		$def['columna'][3]['no_nulo']='1';
+		$def['columna'][4]['nombre']='orden';
+		$def['columna'][5]['nombre']='etiqueta';
+		$def['columna'][6]['nombre']='descripcion';
+		$def['columna'][7]['nombre']='tip';
+		$def['columna'][8]['nombre']='imagen_recurso_origen';
+		$def['columna'][9]['nombre']='imagen';
+		$def['columna'][10]['nombre']='objetos';
+		$def['columna'][11]['nombre']='ev_procesar';
+		$def['columna'][12]['nombre']='ev_cancelar';
+		parent::__construct( $def, $fuente, $min_registros, $max_registros);
+	}	
+	
+	function cargar_datos_clave($id)
+	{
+		$where[] = "objeto_ci_proyecto = '{$id['objeto_ci_proyecto']}'";
+		$where[] = "objeto_ci = '{$id['objeto_ci']}'";
+		$where[] = "pantalla = '{$id['pantalla']}'";
+		$this->cargar_datos($where);
+	}
+}
+?>
---
+++ php/admin/db/db_registros/dbr_apex_objeto_ei_formulario_ef.php
@@ -0,0 +1,48 @@
+<?
+//Generacion: 23-07-2005 05:03:41
+//Fuente de datos: 'instancia'
+require_once('nucleo/persistencia/db_registros_s.php');
+
+class dbr_apex_objeto_ei_formulario_ef extends db_registros_s
+//db_registros especifico de la tabla 'apex_objeto_ei_formulario_ef'
+{
+	function __construct($fuente=null, $min_registros=0, $max_registros=0 )
+	{
+		$def['tabla']='apex_objeto_ei_formulario_ef';
+		$def['columna'][0]['nombre']='objeto_ei_formulario_proyecto';
+		$def['columna'][0]['pk']='1';
+		$def['columna'][0]['no_nulo']='1';
+		$def['columna'][1]['nombre']='objeto_ei_formulario';
+		$def['columna'][1]['pk']='1';
+		$def['columna'][1]['no_nulo']='1';
+		$def['columna'][2]['nombre']='objeto_ei_formulario_fila';
+		$def['columna'][2]['pk']='1';
+		$def['columna'][2]['secuencia']='apex_obj_ei_form_fila_seq';
+		$def['columna'][3]['nombre']='identificador';
+		$def['columna'][3]['no_nulo']='1';
+		$def['columna'][4]['nombre']='elemento_formulario';
+		$def['columna'][4]['no_nulo']='1';
+		$def['columna'][5]['nombre']='columnas';
+		$def['columna'][5]['no_nulo']='1';
+		$def['columna'][6]['nombre']='obligatorio';
+		$def['columna'][7]['nombre']='inicializacion';
+		$def['columna'][8]['nombre']='orden';
+		$def['columna'][8]['no_nulo']='1';
+		$def['columna'][9]['nombre']='etiqueta';
+		$def['columna'][10]['nombre']='descripcion';
+		$def['columna'][11]['nombre']='colapsado';
+		$def['columna'][12]['nombre']='desactivado';
+		$def['columna'][13]['nombre']='estilo';
+		$def['columna'][14]['nombre']='total';
+		parent::__construct( $def, $fuente, $min_registros, $max_registros);
+	}	
+	
+	function cargar_datos_clave($id)
+	{
+		$where[] = "objeto_ei_formulario_proyecto = '{$id['objeto_ei_formulario_proyecto']}'";
+		$where[] = "objeto_ei_formulario = '{$id['objeto_ei_formulario']}'";
+		$where[] = "objeto_ei_formulario_fila = '{$id['objeto_ei_formulario_fila']}'";
+		$this->cargar_datos($where);
+	}
+}
+?>
