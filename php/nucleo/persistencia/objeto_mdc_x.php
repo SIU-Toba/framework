e1aefd1e0c3a5979789424263947a065205d9c1e
Juan Bordón <jbordon@siu.edu.ar>
2005-08-11 17:27:18 +0000

    Orden en la seccion persistencia

--- php/nucleo/persistencia/objeto_mdc_x.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("objeto_mdc.php");
-/*
-	representa a una matriz de datos que provee un mecanismo de carga y persistencia alternativo
-*/
-class objeto_mdc_x extends objeto_mdc
-{
-	
-}
