aca52cafd101392f3c53f4199e5cc7ca755be38d
Sebastián Marconi <smarconi@siu.edu.ar>
2005-11-10 22:58:15 +0000

    Nuevo ci de eventos para el editor del ci (perdón, me olvide de subirlo antes)

---
+++ php/admin/objetos_toba/ci/ci_eventos_del_ci.php
@@ -0,0 +1,40 @@
+<?php
+require_once("admin/objetos_toba/ci_eventos.php");
+
+/**
+ * Wrapper del ci_eventos destinado a manejar las particularidades de un evento en un ci
+ */
+class ci_eventos_del_ci extends ci_eventos 
+{
+
+	function evt__eventos__carga()
+	{
+		$datos = parent::evt__eventos__carga();
+		//Se determina el identificador del evento que entiende actualmente las pantallas
+		$id_ev = $this->get_tabla()->get_fila_columna($this->seleccion_evento_anterior, 'identificador');
+		$pantallas = $this->controlador->get_pantallas_evento($id_ev);
+		$datos['pantallas'] = $pantallas;
+		return $datos;	
+	}
+	
+	/**
+	 *	Se redefine la funcion para sacar informacion sobre las pantallas del evento
+	 */
+	function evt__eventos__modificacion($datos)
+	{
+		//Se determina el identificador del evento que entiende actualmente las pantallas
+		$id_ev = $this->get_tabla()->get_fila_columna($this->seleccion_evento_anterior, 'identificador');
+		//Se actualizan las pantallas para que saquen o agreguen a este evento
+		$this->controlador->set_pantallas_evento($datos['pantallas'], $id_ev);
+		//Comunicacion con el manejo estandar de eventos
+		unset($datos['pantallas']);
+		return parent::evt__eventos__modificacion($datos);
+	}
+	
+	function get_pantallas_posibles()
+	{
+		return $this->controlador->get_pantallas_posibles();
+	}	
+}
+
+?>
