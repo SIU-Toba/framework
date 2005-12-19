04011d66b190b4b352fa8e9b39c33ac06a2c03ca
Sebastián Marconi <smarconi@siu.edu.ar>
2005-12-19 14:25:34 +0000

    * Archivo que faltaba al commit anterior  * Se eliminan algunos objetos huerfanos que tenian estructura incorrecta (producto del merge manual malevolo)

--- sql/pgsql_z02_nucleo_act_seq.sql
+++ sql/pgsql_z02_nucleo_act_seq.sql
@@ -1,3 +1,6 @@
 -- Actualizacion del valor de las secuencias
 
 	SELECT setval('apex_sesion_browser_seq', max(sesion_browser)) FROM apex_sesion_browser;
+	SELECT setval('apex_solicitud_seq', max(solicitud)) FROM apex_solicitud; 
+	SELECT setval('apex_solicitud_observacion_seq', max(solicitud_observacion)) FROM apex_solicitud_observacion; 
+	SELECT setval('apex_solicitud_obj_obs_seq', max(solicitud_obj_observacion)) FROM apex_solicitud_obj_observacion; 
---
+++ sql/secuencias.php
@@ -0,0 +1,48 @@
+<?php
+
+$secuencias = array();
+$secuencias['apex_clase_tipo_seq'] = array('campo' => 'clase_tipo', 'tabla'=> 'apex_clase_tipo');
+$secuencias['apex_columna_estilo_seq'] = array('campo' => 'columna_estilo', 'tabla'=> 'apex_columna_estilo');
+$secuencias['apex_columna_formato_seq'] = array('campo' => 'columna_formato', 'tabla'=> 'apex_columna_formato');
+$secuencias['apex_nota_seq'] = array('campo' => 'nota', 'tabla'=> 'apex_nota');
+$secuencias['apex_nucleo_nota_seq'] = array('campo' => 'nucleo_nota', 'tabla'=> 'apex_nucleo_nota');
+$secuencias['apex_item_nota_seq'] = array('campo' => 'item_nota', 'tabla'=> 'apex_item_nota');
+$secuencias['apex_objeto_nota_seq'] = array('campo' => 'objeto_nota', 'tabla'=> 'apex_objeto_nota');
+$secuencias['apex_clase_nota_seq'] = array('campo' => 'clase_nota', 'tabla'=> 'apex_clase_nota');
+$secuencias['apex_patron_nota_seq'] = array('campo' => 'patron_nota', 'tabla'=> 'apex_patron_nota');
+$secuencias['apex_buffer_seq'] = array('campo' => 'buffer', 'tabla'=> 'apex_buffer');
+$secuencias['apex_item_seq'] = array('campo' => 'item_id', 'tabla' => 'apex_item');
+$secuencias['apex_objeto_seq'] = array('campo' => 'objeto', 'tabla'=> 'apex_objeto');
+$secuencias['apex_log_sistema_seq'] = array('campo' => 'log_sistema', 'tabla'=> 'apex_log_sistema');
+$secuencias['apex_sesion_browser_seq'] = array('campo' => 'sesion_browser', 'tabla'=> 'apex_sesion_browser');
+$secuencias['apex_solicitud_seq'] = array('campo' => 'solicitud', 'tabla'=> 'apex_solicitud');
+$secuencias['apex_solicitud_observacion_seq'] = array('campo' => 'solicitud_observacion', 'tabla'=> 'apex_solicitud_observacion');
+$secuencias['apex_solicitud_obj_obs_seq'] = array('campo' => 'solicitud_obj_observacion', 'tabla'=> 'apex_solicitud_obj_observacion');
+$secuencias['apex_log_error_login_seq'] = array('campo' => 'log_error_login', 'tabla'=> 'apex_log_error_login');
+$secuencias['apex_ap_tarea_tipo_seq'] = array('campo' => 'tarea_tipo', 'tabla'=> 'apex_ap_tarea_tipo');
+$secuencias['apex_ap_tarea_estado_seq'] = array('campo' => 'tarea_estado', 'tabla'=> 'apex_ap_tarea_estado');
+$secuencias['apex_ap_tarea_tema_seq'] = array('campo' => 'tarea_tema', 'tabla'=> 'apex_ap_tarea_tema');
+$secuencias['apex_ap_tarea_seq'] = array('campo' => 'tarea', 'tabla'=> 'apex_ap_tarea');
+$secuencias['apex_objeto_plan_linea_seq'] = array('campo' => 'linea', 'tabla'=> 'apex_objeto_plan_linea');
+$secuencias['apex_patron_msg_seq'] = array('campo' => 'patron_msg', 'tabla'=> 'apex_patron_msg');
+$secuencias['apex_item_msg_seq'] = array('campo' => 'item_msg', 'tabla'=> 'apex_item_msg');
+$secuencias['apex_clase_msg_seq'] = array('campo' => 'clase_msg', 'tabla'=> 'apex_clase_msg');
+$secuencias['apex_objeto_msg_seq'] = array('campo' => 'objeto_msg', 'tabla'=> 'apex_objeto_msg');
+$secuencias['apex_msg_seq'] = array('campo' => 'msg', 'tabla'=> 'apex_msg');
+$secuencias['apex_objeto_dbr_columna_seq'] = array('campo' => 'col_id', 'tabla'=> 'apex_objeto_db_registros_col');
+$secuencias['apex_obj_ei_cuadro_col_seq'] = array('campo' => 'objeto_cuadro_col', 'tabla'=> 'apex_objeto_ei_cuadro_columna');	
+$secuencias['apex_obj_ci_pantalla_seq'] = array('campo' => 'pantalla', 'tabla'=> 'apex_objeto_ci_pantalla');
+$secuencias['apex_obj_ei_form_fila_seq'] = array('campo' => 'objeto_ei_formulario_fila', 'tabla'=> 'apex_objeto_ei_formulario_ef');
+$secuencias['apex_log_objeto_seq'] = array('campo' => 'log_objeto', 'tabla'=> 'apex_log_objeto');
+$secuencias['apex_obj_ei_cuadro_cc_seq'] = array('campo' => 'objeto_cuadro_cc', 'tabla'=> 'apex_objeto_cuadro_cc');	
+$secuencias['apex_objeto_dep_seq'] = array('campo' => 'dep_id', 'tabla'=> 'apex_objeto_dependencias');
+$secuencias['apex_objeto_eventos_seq'] = array('campo' => 'evento_id', 'tabla'=> 'apex_objeto_eventos');
+$secuencias['apex_objeto_datos_rel_asoc_seq'] = array('campo' => 'asoc_id', 'tabla'=> 'apex_objeto_datos_rel_asoc');	
+$secuencias['apex_admin_persistencia_seq'] = array('campo' => 'ap', 'tabla'=> 'apex_admin_persistencia');
+$secuencias['apex_columna_proceso_seq'] = array('campo' => 'columna_proceso', 'tabla'=> 'apex_columna_proceso');
+$secuencias['apex_elemento_infra_input_seq'] = array('campo' => 'entrada', 'tabla'=> 'apex_elemento_infra_input');
+$secuencias['apex_nucleo_tipo_seq'] = array('campo' => 'nucleo_tipo', 'tabla'=> 'apex_nucleo_tipo');
+$secuencias['apex_tp_tarea_seq'] = array('campo' => 'tarea', 'tabla'=> 'apex_tp_tarea');
+$secuencias['apex_tp_tarea_tipo_seq'] = array('campo' => 'tarea_tipo', 'tabla'=> 'apex_tp_tarea_tipo');
+
+?>
