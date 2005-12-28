617cc06cbc1da700a9110785906fbeb5b400ab7e
Sebastián Marconi <smarconi@siu.edu.ar>
2005-12-28 14:21:22 +0000

    * Se incluye la capacidad de linkear las ayudas del editor al wiki y al api offline  * Se incluye una version navegable del API

--- php/nucleo/lib/parser_ayuda.php
+++ php/nucleo/lib/parser_ayuda.php
@@ -38,13 +38,16 @@ class parser_ayuda
 	
 	protected static function parsear_wiki($id, $nombre)
 	{
-		$url = recurso::path_apl().'/doc/wiki/'.$id;
-		return "<a href='$url' target='blank'>$nombre</a>";
+		$url = recurso::path_apl()."/doc/wiki/trac/toba/wiki/$id.html";
+		$img = recurso::imagen_apl("wiki.gif", true);
+		return "<span style='white-space: nowrap'><a href='$url'  target='blank'>$nombre</a>$img</span>";
 	}
 	
 	protected static function parsear_api($id, $nombre)
 	{
-		return "<a href='API$id'>$nombre</a>";		
+		$url = recurso::path_apl()."/doc/api/$id.html";
+		$img = recurso::imagen_apl("api.gif", true);
+		return "<span style='white-space: nowrap'><a href='$url'  target='blank'>$nombre</a>$img</span>";		
 	}
 	
 	protected static function parsear_test($id, $nombre)
