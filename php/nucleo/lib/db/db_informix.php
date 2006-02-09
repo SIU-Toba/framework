07e145b5b9c3fdff0ac90c2716a54dadbf8bcfdb
Sebastián Marconi <smarconi@siu.edu.ar>
2006-02-09 12:57:08 +0000

    Se redefinieron los métodos para el manejo de transacciones para las conexiones a Informix ya que las sentencias son particulares del motor.

--- php/nucleo/lib/db/db_informix.php
+++ php/nucleo/lib/db/db_informix.php
@@ -8,5 +8,26 @@ class db_informix extends db
 		$this->motor = "informix";
 		parent::__construct($profile, $usuario, $clave, $base);
 	}
+	
+	function abrir_transaccion()
+	{
+		$sql = 'BEGIN WORK';
+		$this->ejecutar($sql);
+		toba::get_logger()->debug("************ ABRIR transaccion ($this->base@$this->profile) ****************");
+	}
+	
+	function abortar_transaccion()
+	{
+		$sql = 'ROLLBACK WORK';
+		$this->ejecutar($sql);		
+		toba::get_logger()->debug("************ ABORTAR transaccion ($this->base@$this->profile) ****************"); 
+	}
+	
+	function cerrar_transaccion()
+	{
+		$sql = "COMMIT WORK";
+		$this->ejecutar($sql);		
+		toba::get_logger()->debug("************ CERRAR transaccion ($this->base@$this->profile) ****************"); 
+	}	
 }
 ?>
