56e9ab4f5cfb41100b2b1d1133e84859a7b77c8b
Sebastián Marconi <smarconi@siu.edu.ar>
2006-02-16 12:31:52 +0000

    No había hecho un "toba nucleo exportar_datos"

--- php/modelo/metadatos/apex_solicitud_tipo.sql
+++ php/modelo/metadatos/apex_solicitud_tipo.sql
@@ -1,7 +1,8 @@
-INSERT INTO apex_solicitud_tipo (solicitud_tipo, descripcion, descripcion_corta, icono) VALUES ('browser', 'Solicitud realizada desde un Navegador WEB', 'Solicitudes de BROWSER', 'solic_browser.gif');
+INSERT INTO apex_solicitud_tipo (solicitud_tipo, descripcion, descripcion_corta, icono) VALUES ('browser', '[OBSOLETO] Solicitud realizada desde un Navegador WEB', '[OBSOLETO] Solicitudes de BROWSER', 'solic_browser.gif');
 INSERT INTO apex_solicitud_tipo (solicitud_tipo, descripcion, descripcion_corta, icono) VALUES ('consola', 'Solicitud realizada desde la linea de comandos.', 'Solicitud de CONSOLA', 'solic_consola.gif');
 INSERT INTO apex_solicitud_tipo (solicitud_tipo, descripcion, descripcion_corta, icono) VALUES ('consumidor_html', 'Solicitud consumidor HTML', 'Consumidor de HTML', 'solic_consumidor_html.gif');
 INSERT INTO apex_solicitud_tipo (solicitud_tipo, descripcion, descripcion_corta, icono) VALUES ('fantasma', 'Item que no es solicitado nunca', NULL, NULL);
 INSERT INTO apex_solicitud_tipo (solicitud_tipo, descripcion, descripcion_corta, icono) VALUES ('soap', 'Solicitud SOAP', 'Solicitud SOAP
 ', 'solic_wddx.gif');
 INSERT INTO apex_solicitud_tipo (solicitud_tipo, descripcion, descripcion_corta, icono) VALUES ('wddx', 'Solicitud WDDX. Intercambio de informacion con otra instancia', 'Solicitud WDDX', 'solic_wddx.gif');
+INSERT INTO apex_solicitud_tipo (solicitud_tipo, descripcion, descripcion_corta, icono) VALUES ('web', 'Solicitud WEB', 'Solicitud WEB', 'solic_browser.gif');
