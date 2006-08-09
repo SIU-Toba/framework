9ae23e4dc2778df90a65cba4e4a553f2df0b92d6
Sebastián Marconi <smarconi@siu.edu.ar>
2006-08-09 21:20:49 +0000

    Me habia faltado subir la pantalla en si

---
+++ proyectos/toba_editor/php/objetos_toba/ci/pantalla_edt_pantallas.php
@@ -0,0 +1,37 @@
+<?php
+
+class pantalla_edt_pantallas extends objeto_ei_pantalla 
+{
+	function generar_html_dependencias()
+	{
+		$this->dependencia('pantallas_lista')->generar_html();
+		if ( $this->controlador->hay_pant_sel() ){
+			$datos = $this->controlador->get_datos_pantalla_actual();
+			$nombre_pantalla = "Propiedades de la pantalla '{$datos['etiqueta']}' [{$datos['identificador']}]";
+			echo "<fieldset style='margin-top: 10px; padding: 7px;'><legend>$nombre_pantalla</legend>";			
+			$this->dependencia('pantallas')->generar_html();			
+			echo "<table class='tabla-0'  width='100%'>";
+			echo "<tr>\n";
+			if( $this->controlador->existen_deps() ){
+				echo "<td width='50%' style='vertical-align: top'>\n";
+				echo "<fieldset style='margin: 5px;padding: 5px; FONT-SIZE: 10px; COLOR: #990000;'><legend>Dependencias Asociadas</legend>";			
+				$this->dependencia('pantallas_ei')->generar_html();			
+				echo "</fieldset>";
+				echo "</td>\n";
+			}
+			if( $this->controlador->existen_evts() ){
+				echo "<td style='vertical-align: top'>\n";
+				echo "<fieldset style='margin: 5px; padding: 5px; FONT-SIZE: 10px; COLOR: #990000;'><legend>Eventos Asociados</legend>";			
+				$this->dependencia('pantallas_evt')->generar_html();
+				echo "</fieldset>";
+				echo "</td>\n";
+			}
+			echo "</tr>\n";
+			echo "</table>";
+			$this->generar_boton_evento('aceptar_pantalla');
+			$this->generar_boton_evento('cancelar_pantalla');
+			echo "</fieldset>";
+		}
+	}
+}
+?>
