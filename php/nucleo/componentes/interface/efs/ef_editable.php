914b3c3050462dd94dfc90701c8384aa9d7d8073
Sebastián Marconi <smarconi@siu.edu.ar>
2006-08-23 14:34:16 +0000

    Se amplia el tamano por defecto del editable_numero a 10 caracteres (7 numeros aprox. dependiendo de la puntuacion)

--- php/nucleo/componentes/interface/efs/ef_editable.php
+++ php/nucleo/componentes/interface/efs/ef_editable.php
@@ -143,7 +143,7 @@ class ef_editable_numero extends ef_editable
 	function __construct($padre,$nombre_formulario, $id,$etiqueta,$descripcion,$dato,$obligatorio,$parametros)
 	{
 		$this->estilo = "ef-numero";
-        $parametros['edit_tamano'] = (isset($parametros['edit_tamano'])) ? $parametros['edit_tamano'] : 5;
+        $parametros['edit_tamano'] = (isset($parametros['edit_tamano'])) ? $parametros['edit_tamano'] : 10;
 		if (isset($parametros['edit_rango'])) {
 			$this->cambiar_rango($parametros['edit_rango']);
 		}
