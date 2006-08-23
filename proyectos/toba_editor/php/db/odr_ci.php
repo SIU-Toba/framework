2db0774b1c072b7ac70117812c7d67970ca4f8a7
Sebastián Marconi <smarconi@siu.edu.ar>
2006-08-23 22:47:36 +0000

    Editor: Si se crea un ci sin pantalla, se asume una (#416)

--- proyectos/toba_editor/php/db/odr_ci.php
+++ proyectos/toba_editor/php/db/odr_ci.php
@@ -3,12 +3,22 @@
 class odr_ci extends objeto_datos_relacion 
 {
 
+	function sincronizar()
+	{
+		//--- Si no hay pantalla definida se asume una
+		if ($this->tabla('pantallas')->get_cantidad_filas() == 0) {
+			$fila = array('identificador' => 'pant_inicial', 'etiqueta' => 'Pantalla Inicial');
+			$this->tabla('pantallas')->nueva_fila($fila);
+		}		
+		parent::sincronizar();
+	}
+	
 	/**
 	 * Si hay un evento sobre_fila hay que validar que haya una clave definida
 	 */
 	function evt__validar()
 	{
-		$ids_pantallas = $this->tabla('pantallas')->get_ids_pantallas();
+		$ids_pantallas = $this->tabla('pantallas')->get_ids_pantallas();	
 		foreach ($this->tabla('dependencias')->get_filas() as $dep) {
 			if (in_array($dep['identificador'], $ids_pantallas)) {
 				throw new excepcion_toba_def("El identificador '{$dep['identificador']}' se está usando tanto para una pantalla como para una dependencia.");
