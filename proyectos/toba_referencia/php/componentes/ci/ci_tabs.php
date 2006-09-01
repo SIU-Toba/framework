594affdb9369fe5c3a9a6521d4f4fb947eff6c45
Juan Bordón <jbordon@siu.edu.ar>
2006-09-01 19:03:18 +0000

    * Ocultamiento de tabs en JS y PHP en el proyecto referencia.

---
+++ proyectos/toba_referencia/php/componentes/ci/ci_tabs.php
@@ -0,0 +1,24 @@
+<?php 
+
+class ci_tabs extends toba_ci
+{
+	function extender_objeto_js()
+	{
+		echo "	{$this->objeto_js}.evt__b_ocultar_js = function () {
+			this.dependencia('verticales').mostrar_tab('b', false);
+			this.dependencia('horizontales').mostrar_tab('b', false);
+		} 
+		";
+		echo "	{$this->objeto_js}.evt__b_mostrar_js = function () {
+			this.dependencia('verticales').mostrar_tab('b', true);
+			this.dependencia('horizontales').mostrar_tab('b', true);
+		}";
+	}
+	
+	function evt__c_ocultar_php()
+	{
+		$this->dep('verticales')->pantalla()->eliminar_tab('c');
+		$this->dep('horizontales')->pantalla()->eliminar_tab('c');
+	}
+}
+?>
