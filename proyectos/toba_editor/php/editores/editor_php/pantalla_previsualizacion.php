16103ae829aadd555be30ef7b71a93d1991e93dd
Sebastián Marconi <smarconi@siu.edu.ar>
2006-09-28 22:48:47 +0000

    Layout: Faltaba migrar una pantalla

--- proyectos/toba_editor/php/editores/editor_php/pantalla_previsualizacion.php
+++ proyectos/toba_editor/php/editores/editor_php/pantalla_previsualizacion.php
@@ -9,9 +9,9 @@ class pantalla_previsualizacion extends toba_ei_pantalla
 		$this->codigo = $codigo;
 	}
 	
-	function generar_html_dependencias()
+	function generar_layout()
 	{
-		parent::generar_html_dependencias();
+		parent::generar_layout();
 		if( $this->codigo ) {
 			//ei_separador("ARCHIVO: ". $this->controlador->archivo_php()->nombre());
 			echo "<div style='overflow: auto; height: 300px; width: 550px; padding: 5px; text-align:left; background-color: #ffffff; font-size: 11px;'>";
