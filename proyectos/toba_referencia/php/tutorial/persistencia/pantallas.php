46f242aae364e8473332c764d81300a302fbcd45
Sebastián Marconi <smarconi@siu.edu.ar>
2006-12-05 20:38:50 +0000

    Tutorial: persistencia

--- proyectos/toba_referencia/php/tutorial/persistencia/pantallas.php
+++ proyectos/toba_referencia/php/tutorial/persistencia/pantallas.php
@@ -376,10 +376,132 @@ class pant_api extends pant_tutorial
 {
 	function generar_layout()
 	{
+		$vinculo = toba::vinculador()->crear_vinculo(null, 2658, array(), array('celda_memoria'=>'ejemplo'));		
 		$img = toba_recurso::imagen_proyecto('tutorial/persistencia-api.png');
+		$api = toba_parser_ayuda::parsear_api('Componentes/Persistencia/toba_datos_tabla#sec-method-summary',
+												 'documentación del datos_tabla', 'toba_editor');
 		echo "
 			<img style='float:right;padding: 10px;' src='$img'>
+			<p>
+			Una vez cargada la relación es posible consumir el API para manipular los registros en memoria.
+			Las operaciones clásicas sobre la tabla son:</p>
+			<ul>
+				<li>get_fila(\$clave)
+				<li>nueva_fila(\$registro)
+				<li>modificar_fila(\$clave, \$registro)
+				<li>eliminar_fila(\$clave)
+			</ul>
+			<p>
+			Cuando se conoce de antemano que una tabla en la transacción puede tener un único registro se dispone
+			de dos métodos:</p>
+			<ul>
+				<li>set(\$registro)
+				<li>get()
+			</ul>
+				
+			<p>
+			Muchas de estas primitivas hacen referencia a una <strong>\$clave</strong>, vale notar
+			que este valor no es la clave real del registro en la base sino un valor interno
+			que maneja la tabla. Por ejemplo al crear una nueva_fila se le brinda una clave para 
+			futuras referencias siendo que aún en la base aún no existe esta fila.
+			Existe más información sobre las primitivas en la $api.
+			</p>
+			
+			<p style='clear:both'>
+			Volviendo al ejemplo, podemos ver el código que trabaja sobre los
+			datos básicos y los deportes de una persona,
+			tomado directamente de la <a href='$vinculo' target='_blank'>operación</a>:
+			</p>
 		";
+		$codigo ='
+<?php
+...
+	//-------------------------------------------------------------------
+	//--- Pantalla "persona"
+	// Se sabe de antemano que los datos de una persona es un único registro por lo 
+	// que se trabaja con la api get/set	
+	//-------------------------------------------------------------------
+
+	function conf__form_persona()
+	{
+	  return $this->get_relacion()->tabla("persona")->get();
+	}
+
+	function evt__form_persona__modificacion($registro)
+	{
+		$this->get_relacion()->tabla("persona")->set($registro);
+	}
+	
+	//-------------------------------------------------------------------
+	//--- Pantalla "deportes"
+	//-------------------------------------------------------------------
+
+	//-- Cuadro --
+
+	/**
+	 * El cuadro de deportes contiene todos los registros de deportes disponibles
+	 */
+	function conf__cuadro_deportes()	
+	{
+		return $this->get_relacion()->tabla("deportes")->get_filas();	
+	}
+
+	function evt__cuadro_deportes__seleccion($seleccion) {	
+		$this->s__deporte = $seleccion;
+	}
+
+	//-- Formulario --
+		
+	/**
+	 *	Se carga al formulario con el deporte actualmente seleccionada
+	 */
+	function conf__form_deportes()
+	{
+		if(isset($this->s__deporte)) {	
+			return $this->get_relacion()->tabla("deportes")->get_fila($this->s__deporte);	
+		}
+	}
+
+	/**
+	 *	Se modifica el registro y se limpia el formulario
+	 */
+	function evt__form_deportes__modificacion($registro)
+	{
+		if(isset($this->s__deporte)){
+			$this->get_relacion()->tabla("deportes")->modificar_fila($this->s__deporte, $registro);	
+			$this->evt__form_deportes__cancelar();	
+		}
+	}
+
+	/**
+	 *	Se borra el registro y se limpia el formulario
+	 */	
+	function evt__form_deportes__baja()
+	{
+		if(isset($this->s__deporte)){
+			$this->get_relacion()->tabla("deportes")->eliminar_fila( $this->s__deporte );	
+			$this->evt__form_deportes__cancelar();	
+		}
+	}
+
+	/**
+	 *	Se crea una nueva fila
+	 */		
+	function evt__form_deportes__alta($registro)
+	{
+		$this->get_relacion()->tabla("deportes")->nueva_fila($registro);
+	}
+	
+	function evt__form_deportes__cancelar()
+	{
+		unset($this->s__deporte);
+	}
+...	
+?>
+';
+		echo "<div class='codigo'>";
+		highlight_string($codigo);
+		echo "</div>";		
 	}
 }
 
@@ -390,12 +512,108 @@ class pant_sincronizacion extends pant_tutorial
 	function generar_layout()
 	{
 		$img = toba_recurso::imagen_proyecto('tutorial/persistencia-sincronizacion.png');
+		$logger = toba_recurso::imagen_toba('admin/logger.gif', true);		
 		echo "
 			<img style='float:right;padding: 10px;' src='$img'>
+			<p>
+			Finalmente la transacción finaliza sincronizando con el medio de persistencia, 
+			en este caso la base de datos. Durante este proceso se analizan todos los cambios
+			que se produjeron desde la carga y se arma un plan de sincronización formando
+			los comandos SQL necesarios.
+			
+			</p>
 		";
+		$codigo = '
+<?php
+...
+	/**
+	 * Cuando presiona GUARDAR se sincroniza con la base, se resetea la relacion y se cambia de pantalla
+	 */
+	function evt__procesar()
+	{
+		$this->get_relacion()->sincronizar();
+		$this->get_relacion()->resetear();
+		$this->set_pantalla("seleccion");
+	}	
+
+	/**
+	 * Cuando presiona ELIMINAR se eliminan todos los registros de todas las tablas y 
+	 * se sincroniza con la base (implicito), luego se cambia de pantalla
+	 */
+	function evt__eliminar()
+	{
+		$this->get_relacion()->eliminar();
+		$this->set_pantalla("seleccion");
+	}
+
+	/**
+	 * Cuando se presiona CANCELAR se descartan todos los cambio y se cambia de pantalla
+	 */
+	function evt__cancelar()
+	{
+		$this->get_relacion()->resetear();
+		$this->set_pantalla("seleccion");
+	}
+...
+?>
+		';
+		echo "<div class='codigo' style='clear:both'>";
+		highlight_string($codigo);
+		echo "</div>";	
+?>
+<p>
+Si utilizamos el <?php echo $logger; ?> <strong>visor del logger</strong> durante la sincronización podemos ver los comandos
+que generan los componentes para sincronizar la relación (en este caso se modifica la persona, un deporte y se agrego uno nuevo).
+</p>
+
+<div class='codigo' style="font-size: 80%">
+<ul>
+<li > ************ ABRIR transaccion (toba_trunk@localhost) ****************</li>
+<li > AP: ap_persona_deportes- TABLA: ref_persona_deportes - OBJETO: toba_datos_tabla -- <pre>
+
+registro: 1 - INSERT INTO ref_persona_deportes ( persona, deporte, dia_semana, hora_inicio, hora_fin ) 
+ VALUES (2, 3, 0, 10, 11);</pre></li><li > AP: toba_ap_tabla_db_s- TABLA: ref_persona - OBJETO: toba_datos_tabla -- <pre>
+registro: 0 - UPDATE ref_persona SET nombre = 'Jose Enrique', fecha_nac = '2000-05-08' WHERE ( id = 2 );</pre></li><li > AP: ap_persona_deportes- TABLA: ref_persona_deportes - OBJETO: toba_datos_tabla -- <pre>
+registro: 0 - UPDATE ref_persona_deportes SET persona = 2, deporte = 6, dia_semana = 4, hora_inicio = 17, hora_fin = 22 WHERE ( id = 2 );</pre>
+</li>
+<li > ************ CERRAR transaccion (toba_trunk@localhost) ****************</li>
+</ul>
+</div>
+<?php
 	}
 }
 
+//--------------------------------------------------------
+
+class pant_masinfo extends pant_tutorial 
+{
+	function generar_layout()
+	{
+		$wiki1 = toba_parser_ayuda::parsear_wiki('Referencia/Objetos/Persistencia', 
+													'Introducción a los componentes de persistencia',
+													'toba_editor');
+		$wiki2 = toba_parser_ayuda::parsear_wiki('Referencia/Objetos/datos_tabla', 
+													'Documentación del datos_tabla',
+													'toba_editor');
+		$wiki3 = toba_parser_ayuda::parsear_wiki('Referencia/Objetos/datos_relacion', 
+													'Documentación del datos_relacion',
+													'toba_editor');
+		$api1 = toba_parser_ayuda::parsear_api('Componentes/Persistencia/toba_datos_tabla',
+												 'Primitivas del datos_tabla', 'toba_editor');
+		$api2 = toba_parser_ayuda::parsear_api('Componentes/Persistencia/toba_datos_relacion',
+												 'Primitivas del datos_relacion', 'toba_editor');
+												 
+		echo "
+			<ul>
+				<li>$wiki1
+				<li>$wiki2
+				<li>$wiki3
+				<li>$api1
+				<li>$api2
+			</ul>
+		";
+	}
+}
 
 
 ?>
