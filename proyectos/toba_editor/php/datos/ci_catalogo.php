9326a97e3a3c93add19749297f07372c218da208
Juan Bordón <jbordon@siu.edu.ar>
2007-08-13 16:20:08 +0000

    Detalle en la vista de datos

--- proyectos/toba_editor/php/datos/ci_catalogo.php
+++ proyectos/toba_editor/php/datos/ci_catalogo.php
@@ -3,7 +3,7 @@
 class ci_catalogo extends toba_ci
 {
 	//-----------------------------------------------------------------------------------
-	//---- DEPENDENCIAS -----------------------------------------------------------------
+	//---- DEPENDENCIAS ------------------------------------------------------
 	//-----------------------------------------------------------------------------------
 
 	function conf__fuentes(toba_ei_cuadro $cuadro)
@@ -41,5 +41,51 @@ class ci_catalogo extends toba_ci
 		}
 		$cuadro->set_datos($datos);
 	}
+/*
+	function get_acceso_editores($clase, $id_componente)
+	{
+	}	
+	function acceso_zona($parametros = array())
+	{
+		$parametros[apex_hilo_qs_zona] = $this->proyecto . apex_qs_separador . $this->id;
+		return $parametros;
+	}
+
+	function vinculo_editor($parametros = array())
+	{
+		$editor_item = $this->datos['_info']['clase_editor_item'];
+		$editor_proyecto = $this->datos['_info']['clase_editor_proyecto'];
+		return toba::vinculador()->generar_solicitud( $editor_proyecto, $editor_item, $this->acceso_zona($parametros),
+															false, false, null, true, 'central');
+	}
+		//Editor
+		if (isset($this->datos['_info']['clase_editor_proyecto'])) {
+			$ayuda = null;
+			if (in_array($this->datos['_info']['clase'], toba_info_editores::get_lista_tipo_componentes())) {
+				$metodo = "get_pantallas_".$this->datos['_info']['clase'];
+				$pantallas = call_user_func(array("toba_datos_editores", $metodo));
+				//-- Se incluye un vinculo a cada pantalla encontrada
+				$ayuda = "<div class='editor-lista-vinculos'>";
+				foreach ($pantallas as $pantalla) {
+					$img = ($pantalla['imagen'] != '') ? $pantalla['imagen'] : "objetos/fantasma.gif";
+					$origen = ($pantalla['imagen'] != '') ? $pantalla['imagen_recurso_origen'] : 'apex';
+					$vinculo = $this->vinculo_editor(array('etapa' => $pantalla['identificador']));
+					$tag_img = ($origen == 'apex') ? toba_recurso::imagen_toba($img, true) : toba_recurso::imagen_proyecto($img, true);
+					$ayuda .= '<a href='.$vinculo.' target='.apex_frame_centro.
+								" title='".$pantalla['etiqueta']."'>".
+								$tag_img.
+								'</a> ';
+				}
+				$ayuda .= "</div>";
+				$ayuda = str_replace("'", "\\'", $ayuda);
+			}
+			$iconos[] = array(
+				'imagen' => toba_recurso::imagen_toba("objetos/editar.gif", false),
+				'ayuda' => $ayuda,
+				'vinculo' => $this->vinculo_editor()
+			);
+		}
+		return $iconos;	
+*/
 }
 ?>
