ac8e509b458e2d0b5c4d0c61e0bc4a46c0d096ad
Juan Bordón <jbordon@siu.edu.ar>
2008-04-11 15:08:25 +0000

    Creacion de perfiles de datos: Detalles.

--- proyectos/toba_usuarios/php/perfil_datos/ci_editor.php
+++ proyectos/toba_usuarios/php/perfil_datos/ci_editor.php
@@ -3,6 +3,7 @@ class ci_editor extends toba_ci
 {
 	protected $s__proyecto;
 	protected $s__dimension;
+	protected $datos_dimension;
 	
 	function datos()
 	{
@@ -62,11 +63,17 @@ class ci_editor extends toba_ci
 
 	function conf__pant_dimensiones($pantalla)
 	{
+		$this->datos_dimension = toba_info_editores::get_datos_dimension($this->s__proyecto, $this->s__dimension);
 		//Saco los botones del CI de afuera
 		$pantalla_externa = $this->controlador->pantalla();
 		$pantalla_externa->eliminar_evento('guardar');
 		$pantalla_externa->eliminar_evento('cancelar');
 		$pantalla_externa->eliminar_evento('eliminar');
+		$perfil = $this->datos()->tabla('perfil')->get();
+		$txt = "Perfil de datos: <strong>{$perfil['nombre']}</strong>.<br>";
+		$txt .= "Dimensión: <strong>{$this->datos_dimension['nombre']}</strong>.<br>";
+		$txt .= "Seleccione los elementos que desea habilitar para el perfil.";
+		$this->pantalla()->set_descripcion($txt);
 	}
 
 	//---- FORM elementos --------------------------------------------------------------------
@@ -84,24 +91,23 @@ class ci_editor extends toba_ci
 
 	//---- Buscar data de la dimension --------------------------------------------------------------------
 	
+	function get_elementos_dimension()
+	{
+		$id = explode(',',$this->datos_dimension['col_id']);
+		$desc = explode(',',$this->datos_dimension['col_desc']);
+		$sql = "SELECT " . implode(' || ',$id) . " as clave, " 
+						. implode(' || ',$desc) . " as descripcion
+				FROM {$this->datos_dimension['tabla']}
+				ORDER BY descripcion";
+		$datos = $this->get_db($this->datos_dimension['fuente_datos'])->consultar($sql);
+		return $datos;
+	}
+
 	function get_db($id)
 	{
 		$fuente_datos = toba_admin_fuentes::instancia()->get_fuente( $id,
 																	 $this->s__proyecto );
 		return $fuente_datos->get_db();		
-	}
-
-	function get_elementos_dimension()
-	{
-		$datos = toba_info_editores::get_datos_dimension($this->s__proyecto, $this->s__dimension);
-		$id = explode(',',$datos['col_id']);
-		$desc = explode(',',$datos['col_desc']);
-		$sql = "SELECT " . implode(' || ',$id) . " as clave, " 
-						. implode(' || ',$desc) . " as descripcion
-				FROM {$datos['tabla']}
-				ORDER BY descripcion";
-		$datos = $this->get_db($datos['fuente_datos'])->consultar($sql);
-		return $datos;
 	}
 }
 
