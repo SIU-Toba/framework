4bb58e3c9ac1d3face0680e0de83284b6c92201b
Rodrigo Miranda <rmiranda@siu.edu.ar>
2008-04-23 20:28:24 +0000

    * Se agrega al menu de Auditoria la operacion de desbloqueos de IPs desde toba_instancia.

--- proyectos/toba_usuarios/metadatos/permisos/grupo_acceso__admin.sql
+++ proyectos/toba_usuarios/metadatos/permisos/grupo_acceso__admin.sql
@@ -77,6 +77,12 @@ INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, i
 	NULL, --item_id
 	'3445'  --item
 );
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'toba_usuarios', --proyecto
+	'admin', --usuario_grupo_acc
+	NULL, --item_id
+	'3447'  --item
+);
 --- FIN Grupo de desarrollo 0
 
 --- INICIO Grupo de desarrollo 1
