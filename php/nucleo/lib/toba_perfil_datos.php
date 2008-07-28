8deb5c05a6d4e9e7c005d618df53e7a2acdfb8e2
Juan Bordón <jbordon@siu.edu.ar>
2008-07-28 21:43:44 +0000

    Detalles en el probador de perfiles de datos.

--- php/nucleo/lib/toba_perfil_datos.php
+++ php/nucleo/lib/toba_perfil_datos.php
@@ -423,7 +423,14 @@ class toba_perfil_datos
 				$dimensiones = $this->reconocer_dimensiones_implicadas(array_keys($tablas_gatillo), $fuente_datos);
 				//- ANALISIS -------------------------------
 				$test[$id]['gatillos'] = array_keys($tablas_gatillo);
-				$test[$id]['dimensiones'] = array_keys($dimensiones);
+				/*
+				$dims = array();
+				foreach( $dimensiones as $dim) {
+					$dims[$dim] = $this->info_dimensiones[$fuente_datos][$dim]['nombre'];
+				}
+				$test[$id]['dimensiones'] = $dims;
+				*/
+				$test[$id]['dimensiones'] = $dimensiones;
 				//- WHERE ------------------------
 				$where = array();
 				foreach( $dimensiones as $dimension => $tabla ) {
--- proyectos/toba_editor/php/datos/dimensiones/pant_test_dims.php
+++ proyectos/toba_editor/php/datos/dimensiones/pant_test_dims.php
@@ -23,13 +23,8 @@ class pant_test_dims extends toba_ei_pantalla
 			// Pruebas
 			foreach($pruebas as $id => $prueba) {
 				echo '<hr />';
-				echo '<h1>SQL ' . ($id + 1) . ' (';
-				if(isset($prueba['modificado']) && $prueba['modificado'] ) {
-					echo toba_recurso::imagen_toba('aplicar.png',true);
-				} else {
-					echo toba_recurso::imagen_toba('error.png',true);
-				}
-				echo ')</h1><hr />';
+				echo '<h1>SQL ' . ($id + 1);
+				echo '</h1><hr />';
 				//ei_arbol($prueba);
 				//-- SQL original
 				if(isset($prueba['sql_original'])) {
@@ -45,35 +40,82 @@ class pant_test_dims extends toba_ei_pantalla
 					echo $prueba['sql_modificado'];
 					echo "</div>";
 				}
-				//--
-				if(isset($prueba['gatillos'])) {
-					
-				}
-				//--
-				if(isset($prueba['dimensiones'])) {
-					
-				}
-				//--
-				if(isset($prueba['where'])) {
-					
+				//-- Analisis
+				if(isset($prueba['gatillos']) || isset($prueba['dimensiones']) || isset($prueba['where']) ) {
+					echo "<br>";
+					echo "<div style='width: 95%; padding: 5px; text-align: left; background-color: rgb(200, 200, 200)'>";
+					if(isset($prueba['gatillos'])) {
+						$this->lista($prueba['gatillos'], "GATILLOS reconocidos");
+						echo "<br>";
+					}
+					if(isset($prueba['dimensiones'])) {
+						$this->lista($prueba['dimensiones'], "DIMESIONES reconocidas");
+						echo "<br>";
+					}
+					if(isset($prueba['where'])) {
+						echo "<div style='white-space: pre; padding: 5px; text-align: left; background-color: rgb(255, 220, 0)'>";
+						echo "<strong>WHERE GENERADO</strong>\n\n";
+						foreach($prueba['where'] as $where) {
+							echo $where . "\n";	
+						}
+						echo "</div>";
+					}
+					echo "</div>";
 				}
 				//-- CONTAR filas QUERY
-				if(isset($prueba['query_filas_orig'])) {
-					echo "<br><div style='font-size: 14px; color: white; padding: 2px; width: 350px; text-align: center; background-color: rgb(255, 0, 0)'>";
-					echo "Filas Originales: <strong>" . $prueba['query_filas_orig'] . "</strong>";
-					echo " - Filas Resultantes: <strong>" . $prueba['query_filas_modif'] . "</strong>";
-					echo "</div><br>";
-
-				}
-				//-- QUERY
-				if(isset($prueba['query_datos_orig'])) {
-					$this->tabla($prueba['query_datos_orig'], "QUERY Original");
+				if ( isset($prueba['query_filas_orig']) || isset($prueba['query_datos_orig']) ) {
 					echo "<br>";
-					$this->tabla($prueba['query_datos_modif'], "QUERY Resultante");
+					echo "<div style='width: 95%; padding: 5px; text-align: left; background-color: rgb(200, 200, 200)'>";
+					if(isset($prueba['query_filas_orig'])) {
+						echo "<div style='font-size: 14px; color: white; padding: 2px; width: 350px; text-align: center; background-color: rgb(255, 0, 0)'>";
+						echo "Filas Originales: <strong>" . $prueba['query_filas_orig'] . "</strong>";
+						echo " - Filas Resultantes: <strong>" . $prueba['query_filas_modif'] . "</strong>";
+						echo "</div><br>";
+	
+					}
+					//-- QUERY
+					if(isset($prueba['query_datos_orig'])) {
+						$this->tabla($prueba['query_datos_orig'], "QUERY Original");
+						echo "<br>";
+						$this->tabla($prueba['query_datos_modif'], "QUERY Resultante");
+					}
+					echo "</div>";
 				}
 			}
 			echo "</div>";
 		}	
+	}
+	
+	function lista($valores, $titulo)
+	{
+		echo "<div style=' color:black'>\n";
+		echo "<table style='BORDER-COLLAPSE: collapse;
+							empty-cells: show; 
+							background-color: white;
+							border: 2px solid black;
+							color:black;
+							'>\n";
+		if($titulo){
+			echo "<tr>\n";
+			echo "<td style='font-size: 12px; color: black; text-align: center; font-weight:bold;' colspan='2'>$titulo</td>\n";
+			echo "</tr>\n";
+		}
+		//Cuerpo
+		if($valores) {
+			$estilo_titulo = "border: 1px solid red; color:red; background-color: yellow; text-align: center;";
+			foreach ($valores as $id => $valor){
+				echo "</tr>\n";
+				echo "<td style='$estilo_titulo'>$id</td>\n";
+				echo "<td style='border: 1px solid gray; padding: 2px;' >$valor</td>\n";
+				echo "</tr>\n";
+			}
+		} else {
+			echo "<tr>\n";
+			echo "<td style='border: 1px solid gray; padding: 2px;' colspan='".(count($fila)+1)."'>No hay DATOS!</td>\n";
+			echo "</tr>\n";
+		}
+		echo "</table>";
+		echo "</div>";		
 	}
 
 	function tabla($tabla,$titulo=null)
@@ -88,7 +130,7 @@ class pant_test_dims extends toba_ei_pantalla
 							'>\n";
 		if($titulo){
 			echo "<tr>\n";
-			echo "<td style='font-size: 12px;  ' colspan='".(count($fila)+1)."'>$titulo</td>\n";
+			echo "<td style='font-size: 12px; color: black; text-align: center; font-weight:bold;' colspan='".(count($fila)+1)."'>$titulo</td>\n";
 			echo "</tr>\n";
 		}
 		//Cuerpo
