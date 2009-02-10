fcd61750e2e2cd91b5c77a850a8bec68b863c466
Ricardo Dalinger <rdalinger@siu.edu.ar>
2009-02-10 12:22:06 +0000

    Trunk: (ticket #598)   * Se inicializa el parametro al momento de crear el proyecto   * Se agrego la actualizacion para los proyectos ya creados en la migraciones a toba 1.3

--- php/modelo/migraciones/toba_migracion_1_3_0.php
+++ php/modelo/migraciones/toba_migracion_1_3_0.php
@@ -7,6 +7,17 @@ class toba_migracion_1_3_0 extends toba_migracion
 		$sql = "ALTER TABLE apex_objeto ALTER COLUMN subclase_archivo TYPE VARCHAR(255);";
 		$this->elemento->get_db()->ejecutar($sql);
 	}	
+	
+	/*
+	* Se asigna el valor 30 al campo que controla el tiempo que permanecera abierta una sesion sin tener interaccion.
+	*/	
+	function proyecto__tiempo_no_interac_por_defecto()
+	{
+		$sql = "UPDATE apex_proyecto SET sesion_tiempo_no_interac_min = 30
+				WHERE (sesion_tiempo_no_interac_min IS NULL) OR (sesion_tiempo_no_interac_min = 0);";
+		
+		return $this->elemento->get_db()->ejecutar($sql);
+	}	
 }
 
 ?>
