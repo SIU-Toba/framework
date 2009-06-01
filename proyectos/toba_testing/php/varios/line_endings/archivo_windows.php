dbf1864b1df5aa478cea6085de346f1b2d7e1081
Ricardo Dalinger <rdalinger@siu.edu.ar>
2009-06-01 19:07:57 +0000

    Testeo de EOL: Archivo en windows

---
+++ proyectos/toba_testing/php/varios/line_endings/archivo_windows.php
@@ -0,0 +1,16 @@
+<?php
+echo 'Archivo realizado con Notepad--';
+echo 'Probablemente ni siquiera sepa lo que es un EOL';
+echo '\r\n Deberian transformarse en \n comunes ambos \r\n';
+
+class archivo_windows
+{
+
+	function get_archivo_windows()
+	{
+		echo 'Lucha por el Tab!, tiene que estar en 4 posiciones';
+		echo 'Windows 7 \r\n Sufrimiento x 7';
+	}
+
+}
+?>
