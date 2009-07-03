6d7319bc240fc554baf744761ebf9f9a21400aa9
Sebastián Marconi <smarconi@siu.edu.ar>
2009-07-03 00:17:07 +0000

    Trunk: Mejoras chequeo sincro metadatos  * No estaba haciendo el caso inicial de chequeo  * Tiene en cuenta el caso salida html_parcial (algun dia hay que ponerse en serio para que usar esto sea realmente ortogonal)  * Fallaba cuando se
    El chequeo funciona bien, pero la 1era vez en windows tenes que sentarte a esperar y a veces se bloquea el proceso horriblemente. Otro detalle molesto es que luego de iniciar sesion el chequeo se hace en el pedido que saca el frameset y eso da la sensacion de un login infinito (svn armando el cache) y encima si se detecta un error de sincro no se notifica porque es el frameset.
    
    En conclusión dejaria este chequeo de lado en la 1.4 en el editor, al menos de forma automatica.

--- proyectos/toba_editor/php/customizacion_toba/contexto_ejecucion_editor.php
+++ proyectos/toba_editor/php/customizacion_toba/contexto_ejecucion_editor.php
@@ -3,6 +3,8 @@
 class contexto_ejecucion_editor implements toba_interface_contexto_ejecucion
 {
 	var $tiempo_minimo_control = 30; //tiempo expresado en segundos
+	protected $chequeado = false;
+	
 	function conf__inicial()
 	{
 		require_once('admin_util.php');		
@@ -23,28 +25,38 @@ class contexto_ejecucion_editor implements toba_interface_contexto_ejecucion
 
 	function conf__final()
 	{
-		//Realizo el control sobre las revisiones de codigo
-		$ultimo_control = toba::memoria()->get_dato('ultimo_control_revision');
-		$actual = time();
-		if (is_null($ultimo_control)) { //Para la primera vez que entra
-			$ultimo_control = $actual;
+		if (toba_editor::activado() && ! $this->chequeado) {
+			//Realizo el control sobre las revisiones de codigo
+			$ultimo_control = toba::memoria()->get_dato('ultimo_control_revision');
+			$actual = time();
+			if (is_null($ultimo_control)) { //Para la primera vez que entra
+				$ultimo_control = 0;
+			}
+			//Si pasaron mas de 30 sgs disparo el control
+			if ($ultimo_control < ($actual - $this->tiempo_minimo_control)) {
+				$this->chequear_sincro_svn();
+				//Seteo el ts del ultimo control
+				toba::memoria()->set_dato('ultimo_control_revision', $actual);
+			}
 		}
-		//Si pasaron mas de 30 sgs disparo el control
-		if ($ultimo_control < ($actual - $this->tiempo_minimo_control)) {			
-			$proy = toba_editor::get_modelo_proyecto();
-			try {
-					$proy->chequear_actualizacion_prematura();
-			} catch(toba_error_def $e) {
-					//Hubo update prematuro
-					$msg = $e->getMessage();
-					$msg = str_replace("\n", '<BR>', $msg); //Lo pongo bonito
-					toba::notificacion()->agregar($msg, 'error');
-			} catch(toba_error $e) {}
-			//Seteo el ts del ultimo control
-			toba::memoria()->set_dato('ultimo_control_revision', $actual);
-			//Muestro msg de error si los hubiera
-			toba::notificacion()->mostrar();
-		}	
+	}
+	
+	function chequear_sincro_svn()
+	{
+		$this->chequeado = true;
+		$proy = toba_editor::get_modelo_proyecto();
+		try {
+				$proy->chequear_actualizacion_prematura();
+		} catch(toba_error_def $e) {
+				//Hubo update prematuro
+				$msg = $e->getMessage();
+				$msg = str_replace("\\", "\\\\", $msg);
+				$msg = nl2br($msg);
+				toba::notificacion()->agregar($msg, 'error');
+				//Muestro msg de error si los hubiera
+				$tags_js = toba::memoria()->get_servicio_solicitado() != 'html_parcial';
+				toba::notificacion()->mostrar($tags_js);
+		} catch(toba_error $e) {}		
 	}
 
 }
