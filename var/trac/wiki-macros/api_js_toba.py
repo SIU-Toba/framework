f15393c040d8331e09db8c7bdc0e72f32855cd5e
Sebastián Marconi <smarconi@siu.edu.ar>
2009-07-21 18:56:59 +0000

    Trunk: Nuevo formato de macros de trac 0.11 (que día señor!)

--- var/trac/wiki-macros/api_js_toba.py
+++ var/trac/wiki-macros/api_js_toba.py
@@ -1,20 +1,25 @@
-"""
-    Hace un link al api JS de Toba generado por phpDoc [[BR]]
-    Sintaxis: {{{[[api_js_toba(package/subpackage/clase, seccion_en_clase, texto]]}}} [[BR]]
-    Ej:
-    {{{
-	[[api_js_toba(Objetos/Persistencia/objeto_datos_relacion, methoddump_esquema, Ver API)]]
-    }}}
-    [[api_js_toba(Objetos/Persistencia/objeto_datos_relacion, methoddump_esquema, Ver API)]]
-"""
+from trac.wiki.macros import WikiMacroBase
 
-def execute(hdf, txt, env):
-    args = txt.split(',', 2)
-    clase = args[0].strip()
-    seccion = len(args) > 1 and args[1].strip() or ""
-    descripcion = len(args) > 2 and args[2].strip() or clase
-    url = "/toba_editor_trunk/doc/api_js/%s.html" % (clase)
-    descripcion = "<img style='vertical-align:middle' src='/toba_editor_trunk/doc/api/media/javascript-small.png' /> " + descripcion
-    salida = "<a href='%s#%s' title='Navega hacia la documentacion JAVASCRIPT'>%s</a>" % (url, seccion, descripcion)
-    return salida
-    
+class api_js_toba(WikiMacroBase):
+    """
+        Hace un link al api JS de Toba generado por phpDoc [[BR]]
+        Sintaxis: {{{[[api_js_toba(package/subpackage/clase, seccion_en_clase, texto]]}}} [[BR]]
+        Ej:
+        {{{
+        [[api_js_toba(Objetos/Persistencia/objeto_datos_relacion, methoddump_esquema, Ver API)]]
+        }}}
+        [[api_js_toba(Objetos/Persistencia/objeto_datos_relacion, methoddump_esquema, Ver API)]]
+    """
+    revision = "$Rev$"
+    url = "$URL$"
+
+    def expand_macro(self, formatter, name, txt):
+        args = txt.split(',', 2)
+        clase = args[0].strip()
+        seccion = len(args) > 1 and args[1].strip() or ""
+        descripcion = len(args) > 2 and args[2].strip() or clase
+        url = "/toba_editor_trunk/doc/api_js/%s.html" % (clase)
+        descripcion = "<img style='vertical-align:middle' src='/toba_editor_trunk/doc/api/media/javascript-small.png' /> " + descripcion
+        salida = "<a href='%s#%s' title='Navega hacia la documentacion JAVASCRIPT'>%s</a>" % (url, seccion, descripcion)
+        return salida
+  
--- var/trac/wiki-macros/toba_referencia.py
+++ var/trac/wiki-macros/toba_referencia.py
@@ -1,13 +1,19 @@
-"""
-    Hace un link al api de Toba generado por phpDoc [[BR]]
-    Sintaxis: {{{[[toba_referencia(item, descripcion)]]}}} [[BR]]
-"""
+from trac.wiki.macros import WikiMacroBase
 
-def execute(hdf, txt, env):
-    args = txt.split(',', 2)
-    url = "/toba_referencia_trunk/aplicacion.php?ai=toba_referencia||%s" % (args[0].strip())
-    descripcion = args[1].strip()
-    #descripcion = "<img style='vertical-align:middle' src='/toba_editor_trunk/doc/api/media/php-small.png' />" + descripcion
-    salida = "<a href='%s' title='Navega hacia el ejemplo en el proyecto online de referencia'>%s</a>" % (url, descripcion)
-    return salida
-    
+class toba_referencia(WikiMacroBase):
+    """
+        Hace un link al api de Toba generado por phpDoc [[BR]]
+        Sintaxis: {{{[[toba_referencia(item, descripcion)]]}}} [[BR]]
+    """
+
+    revision = "$Rev$"
+    url = "$URL$"
+
+    def expand_macro(self, formatter, name, txt):
+        args = txt.split(',', 2)
+        url = "/toba_referencia_trunk/aplicacion.php?ai=toba_referencia||%s" % (args[0].strip())
+        descripcion = args[1].strip()
+        #descripcion = "<img style='vertical-align:middle' src='/toba_editor_trunk/doc/api/media/php-small.png' />" + descripcion
+        salida = "<a href='%s' title='Navega hacia el ejemplo en el proyecto online de referencia'>%s</a>" % (url, descripcion)
+        return salida
+   
