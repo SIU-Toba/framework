b5542fc6308b6f4c9a35a6c7d82baf1bd3b2254c
Ricardo Dalinger <rdalinger@siu.edu.ar>
2009-08-06 20:15:50 +0000

    Trunk: Mejora en la edicion de titulos ahora se permite ingresar hasta el maximo de 120 chars

--- proyectos/toba_editor/metadatos/componentes/toba_ei_formulario/dump_1355.sql
+++ proyectos/toba_editor/metadatos/componentes/toba_ei_formulario/dump_1355.sql
@@ -631,15 +631,15 @@ INSERT INTO apex_objeto_ei_formulario_ef (objeto_ei_formulario_fila, objeto_ei_f
 	'Titulo interface', --etiqueta
 	NULL, --etiqueta_estilo
 	NULL, --descripcion
-	NULL, --colapsado
-	NULL, --desactivado
+	'0', --colapsado
+	'0', --desactivado
 	NULL, --estilo
 	NULL, --total
 	NULL, --inicializacion
-	NULL, --permitir_html
-	NULL, --deshabilitar_rest_func
+	'0', --permitir_html
+	'0', --deshabilitar_rest_func
 	NULL, --estado_defecto
-	NULL, --solo_lectura
+	'0', --solo_lectura
 	NULL, --carga_metodo
 	NULL, --carga_clase
 	NULL, --carga_include
@@ -656,7 +656,7 @@ INSERT INTO apex_objeto_ei_formulario_ef (objeto_ei_formulario_fila, objeto_ei_f
 	NULL, --carga_no_seteado
 	NULL, --carga_no_seteado_ocultar
 	'60', --edit_tamano
-	'80', --edit_maximo
+	'120', --edit_maximo
 	NULL, --edit_mascara
 	NULL, --edit_unidad
 	NULL, --edit_rango
