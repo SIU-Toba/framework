cd824d7037370746f23bf8aac1c8bf4bcce16539
Ricardo Dalinger <rdalinger@siu.edu.ar>
2011-06-10 17:49:18 +0000

    Trunk: * La operacion de ABM de usuarios, fallaba xq quedo mal el nombre de la secuencia en el datos_tabla

--- proyectos/toba_usuarios/metadatos/componentes/toba_datos_tabla/dump_33000064.sql
+++ proyectos/toba_usuarios/metadatos/componentes/toba_datos_tabla/dump_33000064.sql
@@ -74,7 +74,7 @@ INSERT INTO apex_objeto_db_registros_col (objeto_proyecto, objeto, col_id, colum
 	'cod_pregunta_secreta', --columna
 	'E', --tipo
 	'1', --pk
-	'apex_usuario_pregunta_secreta_cod_pregunta_secreta_seq', --secuencia
+	'apex_usuario_pregunta_secreta_seq', --secuencia
 	NULL, --largo
 	NULL, --no_nulo
 	'1', --no_nulo_db
