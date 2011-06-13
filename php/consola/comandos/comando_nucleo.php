3b25942d000ffe05eedadf46e6473a80ee47be4e
Ricardo Dalinger <rdalinger@siu.edu.ar>
2011-06-13 14:02:02 +0000

    Trunk: Se agrega la opcion zipversion para el comando toba nucleo.

--- php/consola/comandos/comando_nucleo.php
+++ php/consola/comandos/comando_nucleo.php
@@ -104,6 +104,96 @@ class comando_nucleo extends comando_toba
 	}
 	
 	/**
+	 * Dado un release en trunk_versiones genera los archivos zip y tar.gz correspondientes a la version
+	 * @consola_parametros -r release -u usuario_svn [-s url_svn] [-t path temporal] [-d path destino] 
+	 */
+	function opcion__zipversion()
+	{
+		$url_svn = 'http://repositorio.siu.edu.ar/svn/toba';
+		$dir_temp = '/tmp';
+		$destino = $dir_temp;
+		$rama_versiones = 'versiones';
+		$rama_branch = 'trunk_versiones';
+		
+		//Obtengo los parametros de entrada
+		$param = $this->get_parametros();		
+		if ( !isset($param['-r']) ||  (trim($param['-r']) == '') ) {
+			throw new toba_error("Es necesario indicar el release con '-r'");
+		}
+		if ( !isset($param['-u']) ||  (trim($param['-u']) == '') ) {
+			throw new toba_error("Es necesario indicar el usuario svn con '-u'");
+		}
+		if (isset($param['-s']) && (trim($param['-s']) != '') ) {
+			$url_svn = $param['-s'];
+		}
+		if (isset($param['-t']) && (trim($param['-t']) != '') ) {
+			$dir_temp = $param['t'];
+		}
+		if (isset($param['-d']) && (trim($param['-d']) != '') ) {
+			$destino = $param['d'];
+		}
+		$release = $param['-r'];		
+		$usuario = $param['-u'];
+		
+		//-- Averiguo cual es el siguiente numero
+		$versiones = explode("\n", trim(`svn ls $url_svn/$rama_versiones`));
+		$siguiente = new toba_version($release);
+		if (! $this->consola->dialogo_simple('Generando archivos de version '.$siguiente->__toString(). " a partir del release $release", 's')) {
+			return;
+		}
+
+		//Chequeo si el directorio destino es accesible
+		if (is_writable($dir_temp) === false) {
+			throw new toba_error("El usuario actual no tiene permisos de escritura sobre '$dir_temp'");
+		}
+	
+		//-- Hago el export a una carpeta
+		$this->consola->mensaje("Export a carpeta temporal.", false);
+		$export_dir = $dir_temp."/toba_$siguiente"; 
+		if (file_exists($export_dir)) {
+			toba_manejador_archivos::eliminar_directorio($export_dir);
+		}
+		$cmd = "svn export $url_svn/$rama_versiones/$siguiente $export_dir";
+		exec($cmd, $salida, $error);
+		if ($error) {
+			toba_manejador_archivos::eliminar_directorio($export_dir);
+			throw new toba_error("No fue posible hacer el export. Comando:\n$cmd");
+		}		
+		$this->consola->progreso_fin();
+		
+		//-- Armo el .zip 
+		$this->consola->mensaje("Creando ZIP.", false);
+		if (file_exists("$destino/toba_$siguiente.zip")) {
+			unlink("$destino/toba_$siguiente.zip");
+		}
+		$cmd = "cd $dir_temp; zip -r $destino/toba_$siguiente.zip toba_$siguiente";
+		exec($cmd, $salida, $error);
+		if ($error) {
+			toba_manejador_archivos::eliminar_directorio($export_dir);
+			throw new toba_error("Error armando el .zip. Comando:\n$cmd");
+		}
+		$this->consola->progreso_fin();
+		
+		//-- Armo el .tar.gz
+		$this->consola->mensaje("Creando TAR.GZ.", false);
+		if (file_exists("$destino/toba_$siguiente.tar.gz")) {
+			unlink("$destino/toba_$siguiente.tar.gz");
+		}
+		$cmd = "cd $dir_temp; tar -czvf $destino/toba_$siguiente.tar.gz toba_$siguiente";
+		exec($cmd, $salida, $error);
+		if ($error) {
+			toba_manejador_archivos::eliminar_directorio($export_dir);
+			throw new toba_error("Error armando el .tar.gz. Comando:\n$cmd");
+		}
+		$this->consola->progreso_fin();
+		
+		//-- Borro temporales
+		$this->consola->mensaje("Borrando archivos temporales.", false);
+		toba_manejador_archivos::eliminar_directorio($export_dir);
+		$this->consola->progreso_fin();		
+	}
+		
+	/**
 	 * Dado un release en trunk_versiones genera una version en versiones
 	 * @consola_parametros -r release -u usuario_svn [-s url_svn] [-t path temporal] [-d path destino] 
 	 */
