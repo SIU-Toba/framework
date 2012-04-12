a7e7a7ecec562a535f8775db657417f0ac706a8f
Ricardo Dalinger <rdalinger@siu.edu.ar>
2012-04-12 19:15:21 +0000

    * Trunk:  * Modificacion al script que mueve las tablas de log a un esquema propio.  * Bugfix al metodo que separa los lotes de prueba para perfiles de datos en una tabla propia.

--- php/modelo/migraciones/toba_migracion_2_2_0.php
+++ php/modelo/migraciones/toba_migracion_2_2_0.php
@@ -105,7 +105,22 @@ class toba_migracion_2_2_0 extends toba_migracion
 	
 	function instancia__migracion_tablas_log()
 	{
-		$sql = "SET CONSTRAINTS ALL IMMEDIATE;";
+		$secuencias = array('apex_log_error_login' => 'apex_log_error_login_seq',
+						'apex_log_ip_rechazada' => null,
+						'apex_log_objeto' =>'apex_log_objeto_seq',
+						'apex_log_sistema' => 'apex_log_sistema_seq',
+						'apex_log_tarea' => 'apex_log_tarea_seq',
+						'apex_sesion_browser' => 'apex_sesion_browser_seq',
+						'apex_solicitud' => 'apex_solicitud_seq',
+						'apex_solicitud_browser' => null,
+						'apex_solicitud_consola' => null,
+						'apex_solicitud_cronometro' => null,
+						'apex_solicitud_observacion' => 'apex_solicitud_observacion_seq'
+						);
+		
+		$schema_actual = $this->elemento->get_db()->get_schema();		
+		
+		$sql[] = "SET CONSTRAINTS ALL IMMEDIATE;";
 		$this->elemento->get_db()->ejecutar($sql);
 
 		$this->manejador_interface->mensaje('Migrando las tablas de logs.', false);
@@ -113,21 +128,18 @@ class toba_migracion_2_2_0 extends toba_migracion
 		//Hay que crear el schema para las tablas de log
 		$sql[] = 'CREATE SCHEMA toba_logs;';
 		
-		//Recupero las tablas de log 
-		$log_instancia = toba_db_tablas_instancia::get_lista_global_log();
-		$log_proyecto = toba_db_tablas_instancia::get_lista_proyecto_log();
-		$tablas = array_merge($log_instancia, $log_proyecto);
-		
-		$secuencias = $this->elemento->get_db()->get_secuencia_tablas($tablas);
+		//Creo el script para las tablas
+		$columnas_secuencia = $this->elemento->get_db()->get_secuencia_tablas(array_keys($secuencias));		
 		foreach($secuencias as $tabla => $secuencia) {									//Genero las SQL para mover las tablas de schema
-			$sql[] = 'ALTER TABLE '. $tabla . ' SET SCHEMA toba_logs;' ;
-			$sql[] = 'ALTER SEQUENCE '. $secuencia . ' SET SCHEMA toba_logs;' ;
-			$sql[] = 'ALTER TABLE toba_logs.' . $tabla . " ALTER COLUMN log_objeto SET DEFAULT nextval(('toba_logs.\"$secuencia\"'::text)::regclass);";
+			$sql[] = "ALTER TABLE $schema_actual.$tabla SET SCHEMA toba_logs;" ;
+			if (! is_null($secuencia)) {
+				$columna = $columnas_secuencia[$tabla];				
+				$sql[] = "ALTER SEQUENCE $schema_actual.$secuencia SET SCHEMA toba_logs;" ;
+				$sql[] = 'ALTER TABLE toba_logs.' . $tabla . " ALTER COLUMN $columna SET DEFAULT nextval(('toba_logs.\"$secuencia\"'::text)::regclass);";
+			}
 			$this->manejador_interface->progreso_avanzar();
 		}
-		$this->elemento->get_db()->ejecutar($sql);		
-
-		$sql = "SET CONSTRAINTS ALL DEFERRED;";
+		$sql[] = "SET CONSTRAINTS ALL DEFERRED;";
 		$this->elemento->get_db()->ejecutar($sql);
 		
 		//Despues hay que borrar todos los archivos de log que existen actualmente
@@ -164,19 +176,19 @@ class toba_migracion_2_2_0 extends toba_migracion
 	function proyecto__migrar_lote_pruebas_perfil_datos()
 	{
 		$db = $this->elemento->get_db();
-		$proyecto = $this->elemento->get_id();
-		$sql = "SELECT fuente_datos, usuario, clave, base FROM apex_fuente_datos WHERE usuario ILIKE '%select%' OR  usuario ILIKE '%FROM%' ;" ;
-		$fuentes_a_migrar = $db->consultar($sql);
-		if (! empty($fuentes_a_migrar)) {			
+		$proyecto = $db->quote($this->elemento->get_id());
+		$sql = "SELECT fuente_datos, usuario, clave, base FROM apex_fuente_datos WHERE (usuario ILIKE '%select%' OR  usuario ILIKE '%FROM%')  AND proyecto = $proyecto ;";
+		$fuentes_a_migrar = $db->consultar($sql);		
+		if (! empty($fuentes_a_migrar)) {
 			foreach ($fuentes_a_migrar as $fuente) {
 				//Inserto el registro correspondiente en la tabla nueva
 				$sql_in = 'INSERT INTO apex_perfil_datos_set_prueba (proyecto, fuente_datos, lote, seleccionados, parametros) VALUES (';
-				$sql_in .= $db->quote($proyecto) . ',' .  $db->quote($fuente['fuente_datos']) .',';
+				$sql_in .= $proyecto . ',' .  $db->quote($fuente['fuente_datos']) .',';
 				$sql_in .= $db->quote($fuente['usuario']). ',' . $db->quote($fuente['clave']) . ',' . $db->quote($fuente['base']) . ');';
 				$db->ejecutar($sql_in);
 				
 				//Elimino los valores de la otra tabla, no incide ya que se sacan de bases.ini los reales
-				$sql_up = 'UPDATE apex_fuente_datos SET usuario = NULL, clave = NULL, base = NULL WHERE proyecto = '. $db->quote($proyecto) . ' AND fuente_datos = ' . $db->quote($fuente['fuente_datos']). ';' ;
+				$sql_up = 'UPDATE apex_fuente_datos SET usuario = NULL, clave = NULL, base = NULL WHERE proyecto = '. $proyecto . ' AND fuente_datos = ' . $db->quote($fuente['fuente_datos']). ';' ;
 				$db->ejecutar($sql_up);
 			}
 		}
--- php/modelo/migraciones_instancia/toba_migracion_2_2_0.php
+++ php/modelo/migraciones_instancia/toba_migracion_2_2_0.php
@@ -3,21 +3,32 @@ class toba_migracion_2_2_0 extends toba_migracion
 {
 	function instancia__migracion_tablas_log()
 	{
+		$secuencias = array('apex_log_error_login' => 'apex_log_error_login_seq',
+						'apex_log_ip_rechazada' => null,
+						'apex_log_objeto' =>'apex_log_objeto_seq',
+						'apex_log_sistema' => 'apex_log_sistema_seq',
+						'apex_log_tarea' => 'apex_log_tarea_seq',
+						'apex_sesion_browser' => 'apex_sesion_browser_seq',
+						'apex_solicitud' => 'apex_solicitud_seq',
+						'apex_solicitud_browser' => null,
+						'apex_solicitud_consola' => null,
+						'apex_solicitud_cronometro' => null,
+						'apex_solicitud_observacion' => 'apex_solicitud_observacion_seq'
+						);
+		$schema_actual = $this->elemento->get_db()->get_schema();	
 		$this->manejador_interface->mensaje('Migrando las tablas de logs.', false);
 			
 		//Hay que crear el schema para las tablas de log
 		$sql[] = 'CREATE SCHEMA toba_logs;';
-		
-		//Recupero las tablas de log 
-		$log_instancia = toba_db_tablas_instancia::get_lista_global_log();
-		$log_proyecto = toba_db_tablas_instancia::get_lista_proyecto_log();
-		$tablas = array_merge($log_instancia, $log_proyecto);
-		
-		$secuencias = $this->elemento->get_db()->get_secuencia_tablas($tablas);
+				
+		$columnas_secuencia = $this->elemento->get_db()->get_secuencia_tablas(array_keys($secuencias));
 		foreach($secuencias as $tabla => $secuencia) {									//Genero las SQL para mover las tablas de schema
-			$sql[] = 'ALTER TABLE '. $tabla . ' SET SCHEMA toba_logs;' ;
-			$sql[] = 'ALTER SEQUENCE '. $secuencia . ' SET SCHEMA toba_logs;' ;			
-			$sql[] = 'ALTER TABLE toba_logs.' . $tabla . " ALTER COLUMN log_objeto SET DEFAULT nextval(('toba_logs.\"$secuencia\"'::text)::regclass);";
+			$sql[] = "ALTER TABLE  $schema_actual.$tabla  SET SCHEMA toba_logs;" ;
+			if (! is_null($secuencia)) {
+				$columna = $columnas_secuencia[$tabla];
+				$sql[] = "ALTER SEQUENCE $schema_actual.$secuencia  SET SCHEMA toba_logs;" ;
+				$sql[] = 'ALTER TABLE toba_logs.' . $tabla . " ALTER COLUMN $columna SET DEFAULT nextval(('toba_logs.\"$secuencia\"'::text)::regclass);";
+			}
 			$this->manejador_interface->progreso_avanzar();
 		}
 		$this->elemento->get_db()->ejecutar($sql);		
