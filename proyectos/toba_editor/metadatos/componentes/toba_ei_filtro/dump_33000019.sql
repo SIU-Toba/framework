94c50609a28fbaa4ba343ee000982626ef2c36b4
Ricardo Dalinger <rdalinger@siu.edu.ar>
2012-08-17 15:48:54 +0000

    * Trunk:  * Se agrega en toba_editor la columna mensaje al filtro de la operacion de edicion de mensajes del proyecto

--- proyectos/toba_editor/metadatos/componentes/toba_ei_filtro/dump_33000019.sql
+++ proyectos/toba_editor/metadatos/componentes/toba_ei_filtro/dump_33000019.sql
@@ -300,4 +300,61 @@ INSERT INTO apex_objeto_ei_filtro_col (objeto_ei_filtro_col, objeto_ei_filtro, o
 	NULL, --selec_serializar
 	NULL  --selec_cant_columnas
 );
+INSERT INTO apex_objeto_ei_filtro_col (objeto_ei_filtro_col, objeto_ei_filtro, objeto_ei_filtro_proyecto, tipo, nombre, expresion, etiqueta, descripcion, obligatorio, inicial, orden, estado_defecto, opciones_es_multiple, opciones_ef, carga_metodo, carga_clase, carga_include, carga_dt, carga_consulta_php, carga_sql, carga_fuente, carga_lista, carga_col_clave, carga_col_desc, carga_permite_no_seteado, carga_no_seteado, carga_no_seteado_ocultar, carga_maestros, edit_tamano, edit_maximo, edit_mascara, edit_unidad, edit_rango, edit_expreg, estilo, popup_item, popup_proyecto, popup_editable, popup_ventana, popup_carga_desc_metodo, popup_carga_desc_clase, popup_carga_desc_include, popup_puede_borrar_estado, punto_montaje, check_valor_si, check_valor_no, check_desc_si, check_desc_no, selec_cant_minima, selec_cant_maxima, selec_utilidades, selec_tamano, selec_ancho, selec_serializar, selec_cant_columnas) VALUES (
+	'33000017', --objeto_ei_filtro_col
+	'33000019', --objeto_ei_filtro
+	'toba_editor', --objeto_ei_filtro_proyecto
+	'cadena', --tipo
+	'mensaje_a', --nombre
+	'mensaje_a', --expresion
+	'Mensaje', --etiqueta
+	NULL, --descripcion
+	'0', --obligatorio
+	'0', --inicial
+	'4', --orden
+	NULL, --estado_defecto
+	'0', --opciones_es_multiple
+	NULL, --opciones_ef
+	NULL, --carga_metodo
+	NULL, --carga_clase
+	NULL, --carga_include
+	NULL, --carga_dt
+	NULL, --carga_consulta_php
+	NULL, --carga_sql
+	NULL, --carga_fuente
+	NULL, --carga_lista
+	NULL, --carga_col_clave
+	NULL, --carga_col_desc
+	'0', --carga_permite_no_seteado
+	NULL, --carga_no_seteado
+	NULL, --carga_no_seteado_ocultar
+	NULL, --carga_maestros
+	'50', --edit_tamano
+	'255', --edit_maximo
+	NULL, --edit_mascara
+	NULL, --edit_unidad
+	NULL, --edit_rango
+	NULL, --edit_expreg
+	NULL, --estilo
+	NULL, --popup_item
+	NULL, --popup_proyecto
+	NULL, --popup_editable
+	NULL, --popup_ventana
+	NULL, --popup_carga_desc_metodo
+	NULL, --popup_carga_desc_clase
+	NULL, --popup_carga_desc_include
+	NULL, --popup_puede_borrar_estado
+	NULL, --punto_montaje
+	NULL, --check_valor_si
+	NULL, --check_valor_no
+	NULL, --check_desc_si
+	NULL, --check_desc_no
+	NULL, --selec_cant_minima
+	NULL, --selec_cant_maxima
+	NULL, --selec_utilidades
+	NULL, --selec_tamano
+	NULL, --selec_ancho
+	NULL, --selec_serializar
+	NULL  --selec_cant_columnas
+);
 --- FIN Grupo de desarrollo 33
