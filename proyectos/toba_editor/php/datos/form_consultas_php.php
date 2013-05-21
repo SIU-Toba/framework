34a0fc17033ec1e64ca7b3eee6e1d737284c4e8e
Ricardo Dalinger <rdalinger@siu.edu.ar>
2013-05-21 20:21:40 +0000

    * Trunk:   * Bugfix @rev[5969]

--- proyectos/toba_editor/php/datos/form_consultas_php.php
+++ proyectos/toba_editor/php/datos/form_consultas_php.php
@@ -18,11 +18,11 @@ class form_consultas_php extends toba_ei_formulario
 				}
 			}
 
-			/*{$this->objeto_js}.modificar_vinculo__ef_archivo = function(id_vinculo)
+			{$this->objeto_js}.modificar_vinculo__ef_archivo = function(id_vinculo)
 			{
 				var estado = this.ef('punto_montaje').get_estado();
 				vinculador.agregar_parametros(id_vinculo, {'punto_montaje': estado});
-			}*/
+			}
 
 			{$this->objeto_js}.evt__punto_montaje__procesar = function(inicial)
 			{
