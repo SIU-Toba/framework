765bc28eacf057d4ee3f76a2d429866438f51ec7
Ricardo Dalinger <rdalinger@siu.edu.ar>
2013-09-04 20:20:22 +0000

    * Trunk:  * Actualizacion de metadatos de toba_testing a la version 2.3+

--- proyectos/toba_testing/metadatos/tablas/apex_perfil_datos_set_prueba.sql
+++ proyectos/toba_testing/metadatos/tablas/apex_perfil_datos_set_prueba.sql
@@ -11,5 +11,5 @@ INSERT INTO apex_perfil_datos_set_prueba (proyecto, fuente_datos, lote, seleccio
 									WHERE c.escalafon_1 = e.escalafon_1
 									AND c.escalafon_2 = e.escalafon_2;', --lote
 	'0', --seleccionados
-	'a:9:{s:12:\\"perfil_datos\\";s:1:\\"2\\";s:12:\\"sql_original\\";s:1:\\"0\\";s:19:\\"omitir_no_afectados\\";s:1:\\"0\\";s:7:\\"detalle\\";N;s:16:\\"info_dimensiones\\";s:1:\\"1\\";s:9:\\"sql_where\\";s:1:\\"0\\";s:14:\\"sql_modificado\\";s:1:\\"1\\";s:11:\\"datos_filas\\";s:1:\\"0\\";s:12:\\"datos_listar\\";s:1:\\"1\\";}'  --parametros
+	'a:9:{s:12:\"perfil_datos\";s:1:\"2\";s:12:\"sql_original\";s:1:\"0\";s:19:\"omitir_no_afectados\";s:1:\"0\";s:7:\"detalle\";N;s:16:\"info_dimensiones\";s:1:\"1\";s:9:\"sql_where\";s:1:\"0\";s:14:\"sql_modificado\";s:1:\"1\";s:11:\"datos_filas\";s:1:\"0\";s:12:\"datos_listar\";s:1:\"1\";}'  --parametros
 );
