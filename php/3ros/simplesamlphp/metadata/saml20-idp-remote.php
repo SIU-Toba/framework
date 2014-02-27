6aa33689a3fb4e377ed2ebc292ace1925b2d6472
Sebastián Marconi <smarconi@siu.edu.ar>
2014-02-27 21:18:11 +0000

    Trunk: libreria SP de simplesamlphp

---
+++ php/3ros/simplesamlphp/metadata/saml20-idp-remote.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * SAML 2.0 remote IdP metadata for simpleSAMLphp.
+ *
+ * Remember to remove the IdPs you don't use from this file.
+ *
+ * See: https://rnd.feide.no/content/idp-remote-metadata-reference
+ */
+
+/*
+ * Guest IdP. allows users to sign up and register. Great for testing!
+ */
+$metadata['https://openidp.feide.no'] = array(
+	'name' => array(
+		'en' => 'Feide OpenIdP - guest users',
+		'no' => 'Feide Gjestebrukere',
+	),
+	'description'          => 'Here you can login with your account on Feide RnD OpenID. If you do not already have an account on this identity provider, you can create a new one by following the create new account link and follow the instructions.',
+
+	'SingleSignOnService'  => 'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php',
+	'SingleLogoutService'  => 'https://openidp.feide.no/simplesaml/saml2/idp/SingleLogoutService.php',
+	'certFingerprint'      => 'c9ed4dfb07caf13fc21e0fec1572047eb8a7a4cb'
+);
+
+
+
+$metadata['http://localhost/simplesaml/saml2/idp/metadata.php'] = array(
+	'name' => array(
+		'en' => 'Localhost - guest users'
+	),
+	'description'          => 'Here you can login with your account on Feide RnD OpenID. If you do not already have an account on this identity provider, you can create a new one by following the create new account link and follow the instructions.',
+
+	'SingleSignOnService'  => 'http://localhost/simplesaml/saml2/idp/SSOService.php',
+	'SingleLogoutService'  => 'http://localhost/simplesaml/saml2/idp/SingleLogoutService.php',
+	'certFingerprint'      => 'AF:E7:1C:28:EF:74:0B:C8:74:25:BE:13:A2:26:3D:37:97:1D:A1:F9'
+);
---
+++ php/3ros/simplesamlphp/modules/oauth/config-template/module_oauth.php
@@ -0,0 +1,29 @@
+<?php
+/* 
+ * Configuration for the OAuth module.
+ * 
+ * $Id: module_oauth.php 2843 2011-05-20 14:34:26Z comel.ah $
+ */
+
+$config = array (
+
+	/* Enable the getUserInfo endpoint. Do not enable unless you know what you do.
+	 * It may give external parties access to userInfo unless properly secured.
+	 */
+	'getUserInfo.enable' => TRUE,
+	
+	'requestTokenDuration' => 60*30, // 30 minutes
+	'accessTokenDuration'  => 60*60*24, // 24 hours
+	'nonceCache'           => 60*60*24*14, // 14 days
+
+
+	// Tag to run storage cleanup script using the cron module...
+	'cron_tag' => 'hourly',
+
+	// auth is the idp to use for admin authentication, 
+	// useridattr is the attribute-name that contains the userid as returned from idp
+	'auth' => 'default-sp',
+        'useridattr', 'user',
+
+);
+
