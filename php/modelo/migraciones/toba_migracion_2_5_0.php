5235d29e4517cebe934b1c2e9c7301d5afad7a16
Ricardo Dalinger <rdalinger@siu.edu.ar>
2014-03-21 14:59:28 +0000

    * Trunk:  * Se mueven los cambios a la estructura desde el archivo de migracion 2.6 --> 2.5.

--- php/modelo/migraciones/toba_migracion_2_5_0.php
+++ php/modelo/migraciones/toba_migracion_2_5_0.php
@@ -14,10 +14,49 @@ class toba_migracion_2_5_0 extends toba_migracion
 		$sql[] = 'ALTER TABLE apex_servicio_web ADD COLUMN tipo text DEFAULT "soap";';		
 		$sql[] = 'ALTER TABLE apex_usuario ADD COLUMN forzar_cambio_pwd SMALLINT NOT NULL DEFAULT 0;';
 		$sql[] = 'ALTER TABLE apex_usuario_pwd_usados ADD COLUMN fecha_cambio DATE NOT NULL DEFAULT (\'now\'::text)::date;';
-		$this->elemento->get_db()->ejecutar($sql);
 		
+		//Elimino FK dependientes y PK de apex_menu
+		$sql[] = 'ALTER TABLE apex_proyecto DROP CONSTRAINT "apex_proyecto_fk_menu";';		
+		$sql[] = 'ALTER TABLE apex_menu DROP CONSTRAINT "apex_menu_pk";';					
 		
+		//Renombro tabla y creo nuevamente la PK y FK
+		$sql[] = 'ALTER TABLE apex_menu RENAME TO apex_menu_tipos;';		
+		$sql[] = 'ALTER TABLE apex_menu_tipos ADD CONSTRAINT "apex_menu_tipos_pk" PRIMARY KEY ("menu");';		
+		$sql[] = 'ALTER TABLE apex_proyecto ADD CONSTRAINT "apex_proyecto_fk_menu_tipos"  FOREIGN KEY ("menu") REFERENCES "apex_menu_tipos" ("menu")  ON DELETE NO ACTION ON UPDATE NO ACTION DEFERRABLE INITIALLY IMMEDIATE;';
+		
+		//Creo nueva tabla apex_menu
+		$sql[] = 'CREATE TABLE apex_menu
+				(
+					proyecto						VARCHAR(15)		NOT NULL, 
+					menu_id						VARCHAR(50)		NOT NULL, 
+					descripcion					TEXT			NULL, 
+					tipo_menu					varchar(40)		NOT NULL,
+					CONSTRAINT	"apex_menu_pk"	PRIMARY KEY ("proyecto", "menu_id"), 
+					CONSTRAINT "apex_menu_proyecto_fk"		FOREIGN KEY ("proyecto") REFERENCES "apex_proyecto" ("proyecto") ON DELETE NO ACTION ON UPDATE NO ACTION DEFERRABLE INITIALLY IMMEDIATE,
+					CONSTRAINT "apex_menu_menu_tipos_fk"	FOREIGN KEY ("tipo_menu") REFERENCES "apex_menu_tipos" ("menu") ON DELETE NO ACTION ON UPDATE NO ACTION DEFERRABLE INITIALLY IMMEDIATE
+				);';
 
+		//Creo secuencia y tabla apex_menu_operaciones
+		$sql[] = 'CREATE SEQUENCE apex_menu_operaciones_seq	INCREMENT 1	MINVALUE	1 MAXVALUE 9223372036854775807 CACHE 1;';
+		$sql[] = 'CREATE TABLE apex_menu_operaciones
+				(
+					proyecto						VARCHAR(15)		NOT NULL, 
+					menu_id						VARCHAR(50)		NOT NULL, 
+					menu_elemento				BIGINT			NOT NULL DEFAULT nextval(\'"apex_menu_operaciones_seq"\'::text), 
+					item							VARCHAR(60)		NULL, 
+					padre						VARCHAR(60)		NULL,
+					descripcion					TEXT			NULL,
+					carpeta						SMALLINT		NOT NULL DEFAULT 0,
+					CONSTRAINT	"apex_menu_operaciones_pk"	PRIMARY KEY ("proyecto", "menu_id", "menu_elemento"), 
+					CONSTRAINT "apex_menu_operaciones_apex_proyecto_fk"	FOREIGN KEY ("proyecto") REFERENCES "apex_proyecto" ("proyecto") ON DELETE NO ACTION ON UPDATE NO ACTION DEFERRABLE INITIALLY IMMEDIATE,
+					CONSTRAINT "apex_menu_operaciones_item_fk"	FOREIGN KEY ("proyecto", "item") REFERENCES "apex_item" ("proyecto", "item") ON DELETE NO ACTION  ON UPDATE NO ACTION DEFERRABLE INITIALLY IMMEDIATE, 
+					CONSTRAINT "apex_menu_operaciones_auto_fk" FOREIGN KEY ("proyecto", "menu_id", "menu_elemento") REFERENCES "apex_menu_operaciones" ON DELETE NO ACTION  ON UPDATE NO ACTION DEFERRABLE INITIALLY IMMEDIATE 
+				);';
+
+		$sql[] = 'ALTER TABLE apex_usuario_grupo_acc  ADD CONSTRAINT "apex_usuario_grupo_acc_menu_fk" 
+				FOREIGN KEY (proyecto, menu_usuario) REFERENCES apex_menu (proyecto, menu_id) ON UPDATE NO ACTION ON DELETE NO ACTION DEFERRABLE INITIALLY IMMEDIATE;';
+		
+		$this->elemento->get_db()->ejecutar($sql);
 		
 		$sql = 'SET CONSTRAINTS ALL DEFERRED;';
 		$this->elemento->get_db()->ejecutar($sql);
