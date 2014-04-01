e044bf1ecc7b4df710da1b8e9be50d77e2b51a65
Ricardo Dalinger <rdalinger@siu.edu.ar>
2014-04-01 13:32:47 +0000

    * Trunk:  * Se agregan chequeos para evitar notices por metodos deprecados en 5.4+

--- php/3ros/ezpdf/class.pdf.php
+++ php/3ros/ezpdf/class.pdf.php
@@ -1514,9 +1514,13 @@ function selectFont($fontName,$encoding='',$set=1){
         // simple utility to convert them from pfa to pfb.
         $fp = fopen($fbfile,'rb');
         $tmp = get_magic_quotes_runtime();
-        set_magic_quotes_runtime(0);
+	if(version_compare(PHP_VERSION, '5.3.0', '<')) {		
+			set_magic_quotes_runtime(0);
+	}
         $data = fread($fp,filesize($fbfile));
-        set_magic_quotes_runtime($tmp);
+	if(version_compare(PHP_VERSION, '5.3.0', '<')) {
+			set_magic_quotes_runtime($tmp);
+	}
         fclose($fp);
 
         // create the font descriptor
@@ -2641,7 +2645,9 @@ function addPngFromFile($file,$x,$y,$w=0,$h=0){
   // read in a png file, interpret it, then add to the system
   $error=0;
   $tmp = get_magic_quotes_runtime();
-  set_magic_quotes_runtime(0);
+if(version_compare(PHP_VERSION, '5.3.0', '<')) {		
+	set_magic_quotes_runtime(0);
+}
   $fp = @fopen($file,'rb');
   if ($fp){
     $data='';
@@ -2653,7 +2659,9 @@ function addPngFromFile($file,$x,$y,$w=0,$h=0){
     $error = 1;
     $errormsg = 'trouble opening file: '.$file;
   }
-  set_magic_quotes_runtime($tmp);
+ if(version_compare(PHP_VERSION, '5.3.0', '<')) {
+	set_magic_quotes_runtime($tmp);
+}
   
   if (!$error){
     $header = chr(137).chr(80).chr(78).chr(71).chr(13).chr(10).chr(26).chr(10);
@@ -2856,9 +2864,13 @@ function addJpegFromFile($img,$x,$y,$w=0,$h=0){
   $fp=fopen($img,'rb');
 
   $tmp = get_magic_quotes_runtime();
-  set_magic_quotes_runtime(0);
+if(version_compare(PHP_VERSION, '5.3.0', '<')) {		
+		set_magic_quotes_runtime(0);
+}
   $data = fread($fp,filesize($img));
-  set_magic_quotes_runtime($tmp);
+  if(version_compare(PHP_VERSION, '5.3.0', '<')) {
+	set_magic_quotes_runtime($tmp);
+}
   
   fclose($fp);
 
@@ -2907,7 +2919,9 @@ function addImage(&$img,$x,$y,$w=0,$h=0,$quality=75){
   $fp=fopen($tmpName,'rb');
 
   $tmp = get_magic_quotes_runtime();
-  set_magic_quotes_runtime(0);
+if(version_compare(PHP_VERSION, '5.3.0', '<')) {		
+	set_magic_quotes_runtime(0);
+}
   $fp = @fopen($tmpName,'rb');
   if ($fp){
     $data='';
@@ -2920,7 +2934,9 @@ function addImage(&$img,$x,$y,$w=0,$h=0,$quality=75){
     $errormsg = 'trouble opening file';
   }
 //  $data = fread($fp,filesize($tmpName));
-  set_magic_quotes_runtime($tmp);
+if(version_compare(PHP_VERSION, '5.3.0', '<')) {
+	set_magic_quotes_runtime($tmp);
+}
 //  fclose($fp);
   unlink($tmpName);
   $this->addJpegImage_common($data,$x,$y,$w,$h,$imageWidth,$imageHeight);
