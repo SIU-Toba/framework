8c51e560001802808065a40e4fb6b21827429fb7
Sebastián Marconi <smarconi@siu.edu.ar>
2014-04-03 17:41:23 +0000

    Trunk: se quita acceso a editores menues (queda para la 2.6). En cliente rest se cambia autenticacion de 'Any' a 'digest' porque utiliza un request menos

--- proyectos/toba_usuarios/metadatos/componentes/toba_item/dump_33000034.sql
+++ proyectos/toba_usuarios/metadatos/componentes/toba_item/dump_33000034.sql
@@ -27,7 +27,7 @@ INSERT INTO apex_item (item_id, proyecto, item, padre_id, padre_proyecto, padre,
 	NULL, --descripcion
 	NULL, --punto_montaje
 	NULL, --actividad_accion
-	'1', --menu
+	'0', --menu
 	NULL, --orden
 	NULL, --solicitud_registrar
 	NULL, --solicitud_obs_tipo_proyecto
