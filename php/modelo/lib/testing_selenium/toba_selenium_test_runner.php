2a39674c93c1a927df9c839706c6c4f148f1fb87
Ricardo Dalinger <rdalinger@siu.edu.ar>
2014-06-13 15:50:23 +0000

    Trunk:  * Mejora @rev[6289], ahora los test_case pueden devolver un arreglo de dos componentes (resultado, extra) que se usara para mejorar el reporte a futuro

--- php/modelo/lib/testing_selenium/toba_selenium_test_runner.php
+++ php/modelo/lib/testing_selenium/toba_selenium_test_runner.php
@@ -26,7 +26,13 @@ class toba_selenium_test_runner extends SimpleRunner
 					toba::logger()->debug("\n\n%%%%%%%%%%%--------- INICIO prueba: $method ---------%%%%%%%%%%%%%%%%%%%\n", 'toba');
 				}
 				try {
-					$invoker->invoke($method);
+					$rsp = $invoker->invoke($method);
+					$asrt = $invoker->getTestCase()->get_assert_status();							//Recupero si el caso, usa Assert o no internamente.
+					if (!is_null($rsp) && is_array($rsp)  && !$asrt) {								//En base a ello, uso o no los datos devueltos.
+						$result = (isset($rsp['resultado'])) ? $rsp['resultado'] : false;
+						$msg = $this->parsear_extra_resultado($rsp);
+						$invoker->getTestCase()->assertTrue($result, $msg);					
+					}
 				} catch (PHPWebDriver_WebDriverCurlException $e) {
 					toba::logger()->debug($e->getMessage());
 					throw new toba_error_usuario('Inicie el servidor Selenium por favor');
@@ -38,9 +44,18 @@ class toba_selenium_test_runner extends SimpleRunner
 		}
 	}
 	
-	/*function _isTest($method)
+	/**
+	 * Parsea lo que devuelvan en la componente 'extra' de un arreglo y en base a eso devolvera un msg que podra ser texto, link u otra cosa.
+	 * @param array $datos
+	 * @return text
+	 */
+	protected function parsear_extra_resultado($datos)
 	{
-		return strtolower(substr($method, 0, 4)) == 'case';  //Podria ser test tambien.
-	}*/	
+		$msg = '';
+		if (isset($datos['extra'])) {
+			$msg = (is_array($datos['extra']))? var_export($datos['extra'], true) : $datos['extra'];			
+		}
+		return $msg;
+	}
 }
 ?>
--- php/modelo/lib/testing_selenium/toba_test_selenium.php
+++ php/modelo/lib/testing_selenium/toba_test_selenium.php
@@ -2,6 +2,8 @@
 require_once('PHPWebDriver/__init__.php');
 class toba_test_selenium extends toba_test
 {	
+	protected $usa_assert = true;												//Reemplazar en los hijos si no se va a hacer llamadas a metodos assertX
+	
 	/*
 	*	Crea un runner customizado por toba (agrega separaciones en el LOG por test)
 	*/
@@ -12,7 +14,16 @@ class toba_test_selenium extends toba_test
 			$runner->separar_pruebas(true);
 		}
 		return $runner;
-	}	
+	}
+	
+	/**
+	 *  Define si el test esta haciendo uso de asserts de manerea interna o devuelve el valor al runner.
+	 * @return boolean
+	 */
+	function get_assert_status()
+	{
+		return $this->usa_assert;
+	}
 }
 
 ?>
