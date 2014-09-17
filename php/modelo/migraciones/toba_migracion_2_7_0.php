ca05b0f90a91e9c628893cacc741bda8117478e0
Ricardo Dalinger <rdalinger@siu.edu.ar>
2014-09-17 14:17:06 +0000

    Trunk:  * Faltante @rev[6430]

---
+++ php/modelo/migraciones/toba_migracion_2_7_0.php
@@ -0,0 +1,21 @@
+<?php
+class toba_migracion_2_7_0 extends toba_migracion
+{
+	function instancia__cambios_estructura()
+	{		
+		/**
+		* Se evita el mensaje 'ERROR:  cannot ALTER TABLE "apex_objeto" because
+		* it has pending trigger events' de postgres 8.3
+		*/
+		$sql = 'SET CONSTRAINTS ALL IMMEDIATE;';
+		$this->elemento->get_db()->ejecutar($sql);
+		$sql = array();		
+		
+		$sql[] = 'ALTER TABLE apex_usuario_pwd_usados  DROP CONSTRAINT apex_usuario_pwd_usados_uk;';
+		$this->elemento->get_db()->ejecutar($sql);
+		
+		$sql = 'SET CONSTRAINTS ALL DEFERRED;';
+		$this->elemento->get_db()->ejecutar($sql);
+	}	
+}
+?>
