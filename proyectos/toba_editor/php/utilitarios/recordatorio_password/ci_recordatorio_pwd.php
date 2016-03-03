7ec64bdd53c366a0b5970278bdf4483504722bae
Ricardo Dalinger <rdalinger@siu.edu.ar>
2016-03-03 15:39:26 +0000

    Trunk:  * Bugfix en la recuperacion del desafio

--- proyectos/toba_editor/php/utilitarios/recordatorio_password/ci_recordatorio_pwd.php
+++ proyectos/toba_editor/php/utilitarios/recordatorio_password/ci_recordatorio_pwd.php
@@ -174,8 +174,8 @@ class ci_recordatorio_pwd extends toba_ci
 			$datos = toba::instancia()->get_pregunta_secreta($usuario);
 			if (! is_null($datos)) {
 				$clave = toba::instalacion()->get_claves_encriptacion();        
-				$aux['pregunta'] = mcrypt_decrypt(MCRYPT_BLOWFISH, $clave['get'], $datos['pregunta'], MCRYPT_MODE_CBC, substr($clave['db'], 0, 8));
-				$aux['respuesta'] = mcrypt_decrypt(MCRYPT_BLOWFISH, $clave['get'], $datos['respuesta'], MCRYPT_MODE_CBC, substr($clave['db'], 0, 8));
+				$aux['pregunta'] = mcrypt_decrypt(MCRYPT_BLOWFISH, $clave['get'], base64_decode($datos['pregunta']), MCRYPT_MODE_CBC, substr($clave['db'], 0, 8));
+				$aux['respuesta'] = mcrypt_decrypt(MCRYPT_BLOWFISH, $clave['get'], base64_decode($datos['respuesta']), MCRYPT_MODE_CBC, substr($clave['db'], 0, 8));
 			}
 			return $aux;
 		} catch (toba_error $e) {
