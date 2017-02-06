d36fbbb9add467d13b5371afd0eb994bacb020e0
enfoqueNativo <rdalinger@siu.edu.ar>
2017-02-06 11:26:58 -0300

    * Quito Apache Shindig (quedan clases toba_x por si el proyecto quiere proveerla)

--- proyectos/toba_referencia/php/componentes/toba_gadgets/ci_gadgets.php
+++ proyectos/toba_referencia/php/componentes/toba_gadgets/ci_gadgets.php
@@ -21,13 +21,13 @@ class ci_gadgets extends toba_ci
 		$this->container->agregar_gadgets($this->gadgets_base);
 
 		//Creo un gadget nuevo de manera manual, este no esta en la base de datos
-		$gadget_nuevo = new toba_gadget_shindig();
+		/*$gadget_nuevo = new toba_gadget_shindig();
 		$gadget_nuevo->set_titulo('Gadget Manual');
 		$gadget_nuevo->set_gadget_url('http://www.tc.df.gov.br/MpjTcdf/AlcCalc.xml');
 		$gadget_nuevo->set_orden(3);
 
 		//Lo agrego al contenedor
-		$this->container->agregar_gadgets(array($gadget_nuevo));
+		$this->container->agregar_gadgets(array($gadget_nuevo));*/
 	}
 
 	function get_contenedor()
