a72dd3f7d8ce8f33c21360ef8ef829b36a681999
enfoqueNativo <rdalinger@siu.edu.ar>
2017-07-06 17:36:08 -0300

    - Se agrega el comando instalar al proyecto toba_usuarios para que registre la bd

--- proyectos/toba_usuarios/php/extension_toba/toba_usuarios_modelo.php
+++ proyectos/toba_usuarios/php/extension_toba/toba_usuarios_modelo.php
@@ -5,13 +5,24 @@ class toba_usuarios_modelo extends toba_aplicacion_modelo_base
 	function __construct()
 	{
 		$this->permitir_exportar_modelo = false;
-		$this->permitir_instalar = false;
 	}
 
 	function get_version_nueva()
 	{
 		return $this->get_instalacion()->get_version_actual();
 	}	
+	
+	function instalar($datos_servidor)
+	{
+		if (! $this->permitir_instalar) {
+			return;
+		}		
+		$id_def_base = $this->proyecto->construir_id_def_base($this->get_fuente_defecto());		
+		if (! $this->instalacion->existe_base_datos_definida($id_def_base)) {
+			$datos_servidor = $this->instancia->get_parametros_db();
+			//-- Agrega la definición de la base
+			$this->instalacion->agregar_db($id_def_base, $datos_servidor);
+		}
+	}	
 }
-
 ?>
