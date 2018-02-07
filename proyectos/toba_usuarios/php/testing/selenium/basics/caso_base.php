f049dd0e4303452787c6c48beed81d780f9fac3c
enfoqueNativo <rdalinger@siu.edu.ar>
2018-02-07 15:59:19 -0300

    - Agrega metodo para setear combos

--- proyectos/toba_usuarios/php/testing/selenium/basics/caso_base.php
+++ proyectos/toba_usuarios/php/testing/selenium/basics/caso_base.php
@@ -8,7 +8,6 @@ class caso_base  extends toba_test_selenium
 	{
 		$this->session = basics_proyecto::abrir_browser('chrome');		
 		basics_proyecto::login($this->session);
-		//$url = toba_http::get_url_actual() .toba::vinculador()->get_url(utilidades_testing::get_proyecto_id(), 3432);
 		$url = utilidades_testing::get_url_item(3432);
 		$this->session->get($url);
 		$titulo = $this->session->getTitle();
