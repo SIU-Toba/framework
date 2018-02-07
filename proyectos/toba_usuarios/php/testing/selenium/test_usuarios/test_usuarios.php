ef45c8229db4139b16d0bc936c9d791d903e43d8
enfoqueNativo <rdalinger@siu.edu.ar>
2018-02-07 15:48:59 -0300

    - Agrega y corrige caso de test de usuarios

--- proyectos/toba_usuarios/php/testing/selenium/test_usuarios/test_usuarios.php
+++ proyectos/toba_usuarios/php/testing/selenium/test_usuarios/test_usuarios.php
@@ -1,4 +1,6 @@
 <?php
+	
+
 class test_usuarios extends caso_base 
 {		
 	function test_alta()
@@ -64,17 +66,70 @@ class test_usuarios extends caso_base
 		Titiritero::click_element($session, 'id', 'ci_2188_editor_cambiar_tab_proyecto');
 		$session->wait(2);
 		
-		$botones = Titiritero::get_elementos($session, 'xpath', '//button'); //*[@id="cuadro_2186_cuadro_proyectos1_seleccion"] //*[@id="cuadro_2186_cuadro_proyectos0_seleccion"]
-		var_dump($botones[0]->getText());
-		die;
-		$boton[0]->click();
+		$botones = Titiritero::get_elementos($session, 'xpath', "//button[contains(@name, '_seleccion')]"); //*[@id="cuadro_2186_cuadro_proyectos1_seleccion"] 
+		$botones[1]->click();
 		$session->wait(2);
 		
-		$selects = Titiritero::get_elementos($session, 'id', 'ef_form_2187_form_proyectosusuario_grupo_acc');
-		foreach($selects as $combo) {
-			$combo->selectByIndex(1);
-		}
+		Titiritero::setear_opcion_combo($session, 'xpath', '//select[contains(@name, "usuario_grupo_acc_izq")]', 1);
+		Titiritero::click_element($session, 'xpath', '//select[contains(@name, "usuario_grupo_acc_izq")]//following::img[1]');		
+		$session->wait(1);
 		Titiritero::click_element($session, 'id', 'form_2187_form_proyectos_modificacion');
+		$session->wait(2);
+		Titiritero::click_element($session, 'id', 'ci_2190_guardar');
+		$session->close();
+	}
+	
+	function test_agrego_perfiles()
+	{
+		$session = $this->cargar_operacion_usuarios();
+		Titiritero::setear_campo($session, 'input', 'id', 'ef_form_2189_filtrousuario', 'catman');
+		Titiritero::click_element($session, 'id', 'form_2189_filtro_filtrar');
+		$session->wait(2);
+		
+		Titiritero::click_element($session, 'id', 'cuadro_2181_cuadro0_seleccion');
+		$session->wait(2);
+		
+		$usr_id = current(Titiritero::get_elementos($session, 'id', 'ef_form_2185_basicausuario'))->getAttribute('value');
+		$this->assertTrue($usr_id == 'catman');
+		Titiritero::click_element($session, 'id', 'ci_2188_editor_cambiar_tab_proyecto');
+		$session->wait(2);
+		
+		$botones = Titiritero::get_elementos($session, 'xpath', "//button[contains(@name, '_seleccion')]"); 
+		$botones[1]->click();
+		$session->wait(2);
+		
+		Titiritero::setear_opcion_combo($session, 'xpath', '//select[contains(@name, "usuario_perfil_datos")]', 0);
+		Titiritero::setear_opcion_combo($session, 'xpath', '//select[contains(@name, "usuario_grupo_acc_izq")]', 0);		
+		Titiritero::click_element($session, 'xpath', '//select[contains(@name, "usuario_grupo_acc_izq")]//following::img[1]');		
+		$session->wait(1);
+		Titiritero::click_element($session, 'id', 'form_2187_form_proyectos_modificacion');
+		$session->wait(2);
+		Titiritero::click_element($session, 'id', 'ci_2190_guardar');
+		$session->close();
+	}
+	
+	function test_quito_perfiles_y_permisos()
+	{
+		$session = $this->cargar_operacion_usuarios();
+		Titiritero::setear_campo($session, 'input', 'id', 'ef_form_2189_filtrousuario', 'catman');
+		Titiritero::click_element($session, 'id', 'form_2189_filtro_filtrar');
+		$session->wait(2);
+		
+		Titiritero::click_element($session, 'id', 'cuadro_2181_cuadro0_seleccion');
+		$session->wait(2);
+		
+		$usr_id = current(Titiritero::get_elementos($session, 'id', 'ef_form_2185_basicausuario'))->getAttribute('value');
+		$this->assertTrue($usr_id == 'catman');
+		Titiritero::click_element($session, 'id', 'ci_2188_editor_cambiar_tab_proyecto');
+		$session->wait(2);
+		
+		$botones = Titiritero::get_elementos($session, 'xpath', "//button[contains(@name, '_seleccion')]"); 
+		$botones[1]->click();
+		$session->wait(2);
+				
+		Titiritero::click_element($session, 'id', 'form_2187_form_proyectos_baja');
+		$session->switchTo()->alert()->accept();
+		$session->wait(2);
 		Titiritero::click_element($session, 'id', 'ci_2190_guardar');
 		$session->close();
 	}
@@ -89,10 +144,12 @@ class test_usuarios extends caso_base
 		Titiritero::click_element($session, 'id', 'cuadro_2181_cuadro0_seleccion');
 		$session->wait(3);
 		
-		$usr_id = Titiritero::get_elementos($session, 'id', 'ef_form_2185_basicausuario')->getText();
+		$usr_id = current(Titiritero::get_elementos($session, 'id', 'ef_form_2185_basicausuario'))->getAttribute('value');
 		$this->assertTrue($usr_id == 'catman');
 		
 		Titiritero::click_element($session, 'id', 'ci_2190_eliminar');
+		$session->switchTo()->alert()->accept();
+		$session->wait(2);
 		$session->close();
 	}
 }
