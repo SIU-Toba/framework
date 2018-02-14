5c0b551ef81a7589c0c26fd55d141aa02b247f04
enfoqueNativo <rdalinger@siu.edu.ar>
2018-02-14 14:47:23 -0300

    - Desactiva el manejador bootstrap en toba_usuarios

--- proyectos/toba_usuarios/php/extension_toba/contexto_ejecucion.php
+++ proyectos/toba_usuarios/php/extension_toba/contexto_ejecucion.php
@@ -1,7 +1,7 @@
 <?php
 	
-use SIU\ManejadorSalidaBootstrap\bootstrap_factory;
-use SIU\ManejadorSalidaBootstrap\bootstrap_config;
+//use SIU\ManejadorSalidaBootstrap\bootstrap_factory;
+//use SIU\ManejadorSalidaBootstrap\bootstrap_config;
 
 class contexto_ejecucion implements toba_interface_contexto_ejecucion
 {
@@ -10,12 +10,12 @@ class contexto_ejecucion implements toba_interface_contexto_ejecucion
 		require_once('lib/admin_instancia.php');
 		require_once('lib/consultas_instancia.php');
 		
-		$bootstrap_config = new bootstrap_factory();
+		/*$bootstrap_config = new bootstrap_factory();
 		toba::output()->registrarServicio($bootstrap_config);
 		toba::output()->setProvider('bootstrap');
 		
 		bootstrap_config::setMainColor( '#8B0C73');
-		bootstrap_config::setLogoNombre(toba_recurso::imagen_proyecto('logo.gif', false));
+		bootstrap_config::setLogoNombre(toba_recurso::imagen_proyecto('logo.gif', false));*/
 	}
 	
 	function conf__final()
