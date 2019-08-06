3412508be168d3ffbe5789b274222c0bb4c2b876
enfoqueNativo <rdalinger@siu.edu.ar>
2019-08-06 11:15:48 -0300

    Agrega carga de archivo basico faltante para ldap

--- php/lib/toba_config.php
+++ php/lib/toba_config.php
@@ -2,19 +2,19 @@
 class toba_config
 {
 	protected $basic_files = array( 'instalacion' => 'instalacion.ini' ,
-							'fuentes' => array('bases' => 'bases.ini', 'usuarios' => 'conexiones.ini') , 
-							'rdi' => 'rdi.ini', 
-							'idp' => array('cas' => 'cas.ini' , 'openid' => 'openid.ini' ,  'saml' =>'saml.ini', 'onelogin' => 'saml_onelogin.ini'), 
-							'smtp' => 'smtp.ini',
-							'web_server' => 'web_server.ini'
-							);
-	protected $config_files = array();	
+                                'fuentes' => array('bases' => 'bases.ini', 'usuarios' => 'conexiones.ini') ,
+                                'rdi' => 'rdi.ini',
+                                'idp' => array('cas' => 'cas.ini' , 'openid' => 'openid.ini' ,  'saml' =>'saml.ini', 'onelogin' => 'saml_onelogin.ini', 'ldap' => 'ldap.ini'),
+                                'smtp' => 'smtp.ini',
+                                'web_server' => 'web_server.ini'
+                                );
+	protected $config_files = array();
 	protected $config_values = array();
-	
+
 	function __construct()
-	{		
+	{
 	}
-	
+
 	/**
 	 * Dispara la carga de las configuraciones desde archivos
 	 */
@@ -26,7 +26,7 @@ class toba_config
 		/*echo '<pre>';
 		var_dump($this->config_values);*/
 	}
-	
+
 	/**
 	 * Agrega un path a un archivo de configuración a ser cargado bajo un indice especifico
 	 * @param string $index
@@ -37,7 +37,7 @@ class toba_config
 		if (file_exists($file)) {
 			$this->config_files[$index] = realpath($file);
 		}
-	}	
+	}
 
 	/**
 	 * Resetea los valores de configuración cargados
@@ -47,9 +47,9 @@ class toba_config
 		if (isset($this->config_values) && ! empty($this->config_values)) {
 			unset($this->config_values);
 		}
-		$this->config_values = array();	
-	}	
-	
+		$this->config_values = array();
+	}
+
 	/**
 	 * Retorna un valor de configuracion especifico
 	 * @param string $seccion
@@ -62,7 +62,7 @@ class toba_config
 		if (is_null($seccion)) {
 			$seccion = 'general';
 		}
-		
+
 		if (isset($this->config_values[$seccion])) {
 			if (! is_null($subseccion) && isset($this->config_values[$seccion][$subseccion][$parametro])) {
 				return $this->config_values[$seccion][$subseccion][$parametro];
@@ -72,7 +72,7 @@ class toba_config
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Devuelve una subseccion completa de configuración
 	 * @param string $seccion
@@ -83,17 +83,17 @@ class toba_config
 	{
 		return $this->get_parametro($seccion, null, $subseccion);
 	}
-	
+
 	/**
 	 * Devuelve una seccion completa de configuración
 	 * @param string $seccion
 	 * @return mixed
 	 */
 	function get_seccion($seccion)
-	{		  
+	{
 		return (isset($this->config_values[$seccion])) ? $this->config_values[$seccion] : null;
 	}
-		
+
 	/**
 	 * Define si existe un valor para la configuracion especifica
 	 * @param string $seccion
@@ -105,7 +105,7 @@ class toba_config
 	{
 		return $this->get_parametro($seccion, $subseccion, $parametro) != null;
 	}
-	
+
 	//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 	//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 	/**
@@ -114,7 +114,7 @@ class toba_config
 	protected function load_basics()
 	{
 		$dir_start = toba_nucleo::toba_instalacion_dir();			//Reemplaza a toba::instalacion()->get_path_carpeta_instalacion() para no tener un ciclo que deja mal parada la config de instalacion
-		foreach($this->basic_files as $index => $ini) {				
+		foreach($this->basic_files as $index => $ini) {
 			if (! is_array($ini))  {
 				$filename = $dir_start .'/'.$ini;
 				if (file_exists($filename)) {
@@ -125,7 +125,7 @@ class toba_config
 				foreach($ini as $subindex => $ini_name) {
 					$filename = $dir_start .'/'.$ini_name;
 					if (file_exists($filename)) {
-						$aux[$subindex] = $this->parse_array_values(parse_ini_file($filename, true));						
+						$aux[$subindex] = $this->parse_array_values(parse_ini_file($filename, true));
 					}
 				}
 				$this->add_config($index, $aux);
@@ -144,7 +144,7 @@ class toba_config
 			}
 		}
 	}
-	
+
 	/**
 	 * Agrega un conjunto de valores bajo un indice dado
 	 * @param sting $index
@@ -157,17 +157,17 @@ class toba_config
 		} else {
 			foreach($value_pairs as $key => $valores) {
 				$this->config_values[$index][$key] = $this->parse_array_values($valores);
-			}			
+			}
 		}
 	}
-	
+
 	/**
 	 * Recorre un arreglo parseando sus valores
 	 * @param array $arreglo
 	 * @return array
 	 */
-	protected function parse_array_values($arreglo) 
-	{		
+	protected function parse_array_values($arreglo)
+	{
 		$datos = array();
 		if (! is_array($arreglo)) {
 			$datos = $this->parse_val($arreglo);
@@ -178,7 +178,7 @@ class toba_config
 		}
 		return $datos;
 	}
-	
+
 	/**
 	 * Parsea un valor para verificar si hay un pedido de carga desde el entorno.
 	 * @param mixed $valor
@@ -190,13 +190,13 @@ class toba_config
 		if (substr($valor, 0, 4) == '$env' && substr($valor,-1) == '$') {
 			if (preg_match('/^\$env\((.*)\)\$$/', $valor, $matches) !== false) {
 				$var_name = (is_array($matches) && ! empty($matches)) ? $matches[1]: '';
-				return self::load_from_env($var_name);			
+				return self::load_from_env($var_name);
 			}
 		} else {
 			return $valor;
 		}
 	}
-	
+
 	/**
 	 * Levanta un valor desde el entorno devuelve false si no existe o el nombre viene null
 	 * @param string $name
@@ -208,6 +208,6 @@ class toba_config
 			return getenv($name);
 		}
 		return false;
-	}	
+	}
 }
 ?>
