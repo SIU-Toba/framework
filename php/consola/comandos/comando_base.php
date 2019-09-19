7751e8501c6d8ea7398a4cc2c8fc8db330144755
enfoqueNativo <rdalinger@siu.edu.ar>
2019-09-19 17:18:43 -0300

    Fixea notice y agrega posibilidad de especificar encoding al registrar la bd

--- php/consola/comandos/comando_base.php
+++ php/consola/comandos/comando_base.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Clase que implementa el comando sobre la bd.
- * 
+ *
  * Class comando_base.
  * @package consola
  */
@@ -10,7 +10,7 @@ class comando_base extends comando_toba
 {
         /**
          * Retorna información acerca del comando
-         * 
+         *
          * @return string
          */
         static function get_info()
@@ -20,17 +20,17 @@ class comando_base extends comando_toba
 
         /**
          * Muestra un help de uso
-         * 
+         *
          */
         function mostrar_observaciones()
         {
                 $this->consola->mensaje("INVOCACION: toba base OPCION [-d id_base]");
                 $this->consola->enter();
-        }	
+        }
 
         /**
          * Determina si existe el parametro que indica sobre que base operar
-         * 
+         *
          * @return boolean
          */
         function tiene_definido_base()
@@ -39,13 +39,13 @@ class comando_base extends comando_toba
                 if ( isset($param['-d']) &&  (trim($param['-d']) != '') ) {
                         return true;
                 } else {
-                        return false;		
+                        return false;
                 }
         }
 
         /**
          * Devuelve un string con los parametros de la base
-         * 
+         *
          * @return string
          */
         function get_info_extra()
@@ -55,7 +55,7 @@ class comando_base extends comando_toba
                         $param = $this->get_instalacion()->get_parametros_base( $db );
                         $salida = "";
                         foreach ($param as $id => $valor) {
-                                $salida .= "$id: $valor\n";	
+                                $salida .= "$id: $valor\n";
                         }
                         return $salida;
                 }
@@ -63,7 +63,7 @@ class comando_base extends comando_toba
 
         /**
          * Muestra un listado de las bases disponibles
-         * 
+         *
          * @gtk_icono info_chico.gif
          */
         function opcion__listar()
@@ -73,17 +73,17 @@ class comando_base extends comando_toba
 
 
         /**
-         * Agrega la definición de una base al archivo bases.ini. 
-         * 
-         * @param array $parametros 
+         * Agrega la definición de una base al archivo bases.ini.
+         *
+         * @param array $parametros
          * @consola_parametros Opcionales: [-h 'ubicacion bd'] [-p 'puerto'] [-u 'usuario bd'] [-b nombre bd] [-c 'archivo clave bd'] [--schema-toba 'schemaname'] [-o base_origen] toma los datos de otra definicion
-         * @gtk_icono nucleo/agregar.gif  
+         * @gtk_icono nucleo/agregar.gif
          * @gtk_param_extra registrar_base
          */
         function opcion__registrar($parametros=null)
         {
                 if (isset($parametros)) {
-                        list($def, $origen, $datos) = $parametros;			 
+                        list($def, $origen, $datos) = $parametros;
                 }
                 //--- Nombre del registro
                 if (!isset($def)) {
@@ -114,14 +114,14 @@ class comando_base extends comando_toba
                 if (! isset($datos['encoding']) || trim($datos['encoding']) == '') {
                         $this->get_instalacion()->determinar_encoding( $def );
                 }
-        }	
+        }
 
 
         /**
          * Elimina la definición de la base en bases.ini
-         * 
+         *
          * @gtk_icono borrar.gif
-         * @gtk_separador 1 
+         * @gtk_separador 1
          */
         function opcion__desregistrar()
         {
@@ -137,16 +137,16 @@ class comando_base extends comando_toba
                             $i->eliminar_db( $def );
                         } elseif ($this->consola->dialogo_simple("Desea eliminar la definicion?")) {
                             $i->eliminar_db( $def );
-                        }                                                   
+                        }
                 } else {
                         throw new toba_error( "NO EXISTE una base definida con el ID '$def'");
                 }
-        }	
+        }
 
 
         /**
         * Crea físicamente la base de datos
-         * 
+         *
         * @gtk_icono nucleo/agregar.gif
         */
         function opcion__crear()
@@ -162,7 +162,7 @@ class comando_base extends comando_toba
 
         /**
         * Elimina físicamente la base de datos
-         * 
+         *
         * @gtk_icono borrar.png
         */
         function opcion__eliminar()
@@ -183,7 +183,7 @@ class comando_base extends comando_toba
 
         /**
         * Ejecuta un archivo sql
-        * 
+        *
         * @param string $archivo
         * @consola_parametros [-a archivo]
         * @gtk_icono sql.gif
@@ -201,11 +201,11 @@ class comando_base extends comando_toba
                 }
                 $db = $this->get_instalacion()->conectar_base($this->get_id_base_actual());
                 $db->ejecutar_archivo($archivo);
-        }		
+        }
 
         /**
         * Chequea la conexión con la base
-        * 
+        *
         * @gtk_icono fuente.png
         */
         function opcion__test_conexion()
@@ -221,19 +221,19 @@ class comando_base extends comando_toba
 
         /**
          * Actualiza las secuencias de la base, solo funciona con PostgreSQL
-         * 
+         *
          */
         function opcion__actualizar_secuencias()
         {
-                $this->consola->mensaje("Actualizando secuencias", false);		
+                $this->consola->mensaje("Actualizando secuencias", false);
                 $db = $this->get_instalacion()->conectar_base($this->get_id_base_actual());
                 $secuencias = $db->get_lista_secuencias();
                 $db->abrir_transaccion();
                 foreach ($secuencias as $datos) {
-                        $sql_nuevo = "SELECT 
-                                                                max(CASE {$datos['campo']}::varchar ~ '^[0-9]+$' WHEN true THEN {$datos['campo']}::bigint ELSE 0 END) as nuevo		
+                        $sql_nuevo = "SELECT
+                                                                max(CASE {$datos['campo']}::varchar ~ '^[0-9]+$' WHEN true THEN {$datos['campo']}::bigint ELSE 0 END) as nuevo
                                                   FROM {$datos['tabla']}
-                        ";			
+                        ";
                         $res = $db->consultar($sql_nuevo, null, true);
                         $nuevo = $res[0]['nuevo'];
                         //Si no hay un maximo, es el primero del grupo
@@ -242,17 +242,17 @@ class comando_base extends comando_toba
                         }
 
                         $sql = "SELECT setval('{$datos['nombre']}', $nuevo)";
-                        $db->consultar( $sql );				
+                        $db->consultar( $sql );
                         $this->consola->progreso_avanzar();
                 }
-                $db->cerrar_transaccion();		
+                $db->cerrar_transaccion();
                 $this->consola->progreso_fin();
         }
 
 
         /**
         * Determina sobre que base definida en 'info_bases' se va a trabajar
-        * 
+        *
         */
         private function get_id_base_actual()
         {
@@ -262,13 +262,13 @@ class comando_base extends comando_toba
                 } else {
                         throw new toba_error("Es necesario indicar el ID de la definición de la base. Utilice el modificador '-d'");
                 }
-        }	
+        }
 
         private function definir_parametros_base()
         {
-             $param = $this->get_parametros();                        
+             $param = $this->get_parametros();
              $datos = array();
-                 //---- Datos	
+                 //---- Datos
              $form = $this->consola->get_formulario("Definir una nueva BASE de DATOS");
              $datos['motor'] = $this->definir_motor($param, $form);
              $datos['profile'] = $this->definir_profile_motor($param, $form);
@@ -276,35 +276,40 @@ class comando_base extends comando_toba
              $datos['clave'] = $this->definir_clave_motor($param, $form);
              $datos['base'] = $this->definir_base_motor($param, $form);
              $datos['puerto'] = $this->definir_puerto_motor($param, $form);
-             $datos['schema'] = $this->definir_schema_toba($param, $this->id_instancia_actual,  $form);                        
+             $datos['schema'] = $this->definir_schema_toba($param, $this->id_instancia_actual,  $form);
+
+             $encoding_fijado = $this->definir_encoding($param);
+             if (! is_null($encoding_fijado)) {
+                 $datos['encoding'] = $encoding_fijado;
+             }
 
              if (isset($param['--no-interactivo'])) {
                  $form->desactivar_confirmacion_datos();
              }
-             
+
              if (empty($datos) || $form->tiene_campos()) {
                  $datos = array_merge($datos, $form->procesar());
              }
-             
+
              return $datos;
-        }                   
+        }
 
         protected function definir_motor($param, $form=null)
         {
-             $nombre_parametro = array( '-m', '--motor', 'toba-base-motor');    
-             $result = $this->recuperar_dato_y_validez($param, $nombre_parametro);		                
+             $nombre_parametro = array( '-m', '--motor', 'toba-base-motor');
+             $result = $this->recuperar_dato_y_validez($param, $nombre_parametro);
              if ($result['invalido']) {
                      if (! is_null($form)) {
                          $form->agregar_campo( array( 'id' => 'motor',  'nombre' => 'MOTOR (ej. postgres7)' ));
                      } else {
                          return 'postgres7';
                      }
-             }		
+             }
              return $result['resultado'];
         }
 
         protected function definir_schema_toba($param, $id_instancia, $form=null)
-        {		
+        {
              $nombre_parametro = array('--schema-toba');
              $result = $this->recuperar_dato_y_validez($param, $nombre_parametro);
              if ($result['invalido']) {
@@ -314,87 +319,95 @@ class comando_base extends comando_toba
                          return $id_instancia;
                      }
              }
-             return $result['resultado'];		
+             return $result['resultado'];
         }
 
         protected function definir_profile_motor($param, $form=null)
         {
             $nombre_parametro = array('-h', '--base-profile', 'toba-base-profile');
-            $result = $this->recuperar_dato_y_validez($param, $nombre_parametro);		
+            $result = $this->recuperar_dato_y_validez($param, $nombre_parametro);
             if ($result['invalido']) {
                 if (! is_null($form)) {
                     $form->agregar_campo( array( 'id' => 'profile',	'nombre' => 'HOST/PROFILE (ej. localhost)' ));
                 } else {
                     return '127.0.0.1';
                 }
-            }		
+            }
             return $result['resultado'];
         }
 
         protected function definir_usuario_motor($param, $form=null)
         {
             $nombre_parametro = array('-u', '--base-usuario', 'toba-base-usuario');
-            $result = $this->recuperar_dato_y_validez($param, $nombre_parametro);		
+            $result = $this->recuperar_dato_y_validez($param, $nombre_parametro);
             if ($result['invalido']) {
                 if (! is_null($form)) {
                   $form->agregar_campo( array( 'id' => 'usuario', 'nombre' => 'USUARIO (ej. postgres)' ));
                 } else {
                    return 'postgres';
-                }                                            
-            }		
+                }
+            }
             return $result['resultado'];
-        }	
+        }
 
         protected function definir_base_motor($param, $form=null)
         {
             $nombre_toba = 'toba_'.toba_modelo_instalacion::get_version_actual()->get_release('_');
             $nombre_parametro = array('-b', '--base-nombre', 'toba-base-nombre');
-            $result = $this->recuperar_dato_y_validez($param, $nombre_parametro);		
+            $result = $this->recuperar_dato_y_validez($param, $nombre_parametro);
             if ($result['invalido']) {
                 if (! is_null($form)) {
                   $form->agregar_campo( array( 'id' => 'base', 	'nombre' => 'BASE' ));
                 } else {
                   return $nombre_toba;
-                }                                            
-            }		
+                }
+            }
             return $result['resultado'];
         }
 
         protected function definir_puerto_motor($param, $form=null)
         {
             $nombre_parametro = array('-p', '--base-puerto', 'toba-base-puerto');
-            $result = $this->recuperar_dato_y_validez($param, $nombre_parametro);		
-            if ($result['invalido']) {			
+            $result = $this->recuperar_dato_y_validez($param, $nombre_parametro);
+            if ($result['invalido']) {
                 return '5432';
-            }		
+            }
             return $result['resultado'];
-        }	
+        }
 
         protected function definir_clave_motor($param, $form=null)
         {
             $nombre_parametro = array('-c', '--archivo-clave-bd', 'toba-archivo-clave-bd');
+            $clave = null;
             do {
                  $ind = current($nombre_parametro);
                  $es_invalido = (! isset($param[$ind]));
                  if (! $es_invalido) {
-                         $clave = $this->recuperar_contenido_archivo($param[$ind]);			
-                 }	
-            } while($es_invalido && next($nombre_parametro) !== false);		
-            
+                         $clave = $this->recuperar_contenido_archivo($param[$ind]);
+                 }
+            } while($es_invalido && next($nombre_parametro) !== false);
+
             if ($es_invalido) {
                 if (! is_null($form)) {
                          $form->agregar_campo( array( 'id' => 'clave', 'nombre' => 'CLAVE', 'obligatorio' => false ));
                  } else {
-                     return 'postgres';
+                     $clave = 'postgres';
                 }
-            }		
+            }
             return $clave;
+        }
+
+        protected function definir_encoding($param, $form=null)
+        {
+            $nombre_parametro = array('-e', '--encoding-bd', 'toba-encoding-bd');
+            $result = $this->recuperar_dato_y_validez($param, $nombre_parametro);
+            return (! $result['invalido']) ? $result['resultado']: null;
         }
 
         protected function recuperar_dato_y_validez($param, $nombre_parametro)
         {
                 $resultado = null;
-                do {			
+                do {
                         $ind = current($nombre_parametro);
                         $es_invalido = (! isset($param[$ind]));
                         if (! $es_invalido) {
@@ -404,7 +417,7 @@ class comando_base extends comando_toba
 
                 return array('invalido' => $es_invalido, 'resultado' => $resultado);
         }
-        
+
         function recuperar_contenido_archivo($nombre)
         {
                 $resultado = '';
