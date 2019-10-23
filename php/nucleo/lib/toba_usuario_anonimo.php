970929dc588d7953973cde37c2da3b7f82ff837c
Ricardo Dalinger <rdalinger@siu.edu.ar>
2019-10-23 16:58:11 -0300

    Agrega funciones para segundo factor (requerimiento y verificacion)

--- php/nucleo/lib/toba_usuario_anonimo.php
+++ php/nucleo/lib/toba_usuario_anonimo.php
@@ -15,15 +15,25 @@ class toba_usuario_anonimo extends toba_usuario
 	{
 		return toba::proyecto()->get_parametro('usuario_anonimo_desc');
 	}
-	
+
 	function get_perfiles_funcionales()
 	{
 		return toba::proyecto()->get_perfiles_funcionales_usuario_anonimo();
 	}
-	
+
 	function get_perfiles_datos()
 	{
 		return array();
 	}
+
+        function verificar_segundo_factor($clave)
+        {
+            throw \Exception('Metodo no implementado en toba_usuario_anonimo');
+        }
+
+        function requiere_segundo_factor()
+        {
+            return true;
+        }
 }
 ?>
