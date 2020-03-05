7ec9f20e44e80506d3aad5d69d1bc75d26056e53
Ricardo Dalinger <rdalinger@siu.edu.ar>
2020-03-05 15:09:06 -0300

    Fix, agrega lectura del save_path de la sesion al construir el handler

--- php/nucleo/lib/session_handlers/toba_session_files_handler.php
+++ php/nucleo/lib/session_handlers/toba_session_files_handler.php
@@ -5,6 +5,12 @@ class toba_session_files_handler extends toba_session_handler
         'session.save_handler' => 'files',
         'session.save_path' => '/var/lib/php/sessions/'
     );
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->default_settings['session.save_path'] = \session_save_path();
+    }
 }
 ?>
 
