bff30397ccc77a28f704015818bcdfa4a5f46cb7
Ricardo Dalinger <rdalinger@siu.edu.ar>
2023-02-23 13:33:15 -0300

    Pasa CS-Fixer para homogeneizar endlines

--- proyectos/toba_editor/php/configuracion/ci_catalogo.php
+++ proyectos/toba_editor/php/configuracion/ci_catalogo.php
@@ -1,29 +1,28 @@
-<?php 
+<?php
+
 require_once(toba_dir() . '/php/contrib/lib/toba_nodo_basico.php');
 require_once('configuracion/catalogo/catalogo_general.php');
 require_once('configuracion/catalogo/catalogo_perfiles.php');
 
 class ci_catalogo extends toba_ci
 {
-	//-----------------------------------------------------------------------------------
-	//---- DEPENDENCIAS -----------------------------------------------------------------
-	//-----------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------
+    //---- DEPENDENCIAS -----------------------------------------------------------------
+    //-----------------------------------------------------------------------------------
 
-	//---- arbol ------------------------------------------------------------------------
+    //---- arbol ------------------------------------------------------------------------
 
-	function evt__arbol__cambio_apertura($apertura)
-	{
-	}
+    public function evt__arbol__cambio_apertura($apertura)
+    {
+    }
 
-	function evt__arbol__ver_propiedades($nodo)
-	{
-	}
+    public function evt__arbol__ver_propiedades($nodo)
+    {
+    }
 
-	function conf__arbol(toba_ei_arbol $componente)
-	{
-		$catalogo = new catalogo_general();
-		$componente->set_datos($catalogo->get_hijos());
-	}
+    public function conf__arbol(toba_ei_arbol $componente)
+    {
+        $catalogo = new catalogo_general();
+        $componente->set_datos($catalogo->get_hijos());
+    }
 }
-
-?>
