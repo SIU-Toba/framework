9497a362feeb8358570f3681a7e3be8a22efd90c
enfoqueNativo <enfoqueNativo@users.noreply.github.com>
2023-04-27 14:06:45 -0300

    Actualiza campo IP en logs  (#97)
    * Actualiza el tipo de dato de las tablas de logs de Toba

--- php/modelo/migraciones_instancia/toba_migracion_3_4_0.php
+++ php/modelo/migraciones_instancia/toba_migracion_3_4_0.php
@@ -1,24 +1,33 @@
 <?php
+
 class toba_migracion_3_4_0 extends toba_migracion
 {
-	function instancia__cambios_estructura()
-	{
-		/**
-		* Se evita el mensaje 'ERROR:  cannot ALTER TABLE "apex_objeto" because
-		* it has pending trigger events' de postgres 8.3
-		*/
-		$sql = 'SET CONSTRAINTS ALL IMMEDIATE;';
-		$this->elemento->get_db()->ejecutar($sql);
-		$sql = array();
+    public function instancia__cambios_estructura()
+    {
+        /**
+         * Cambia el schema a los logs
+         */
+        $schema_logs = $this->elemento->get_db()->get_schema() . '_logs';
 
-		$sql[] = 'ALTER TABLE apex_usuario ADD COLUMN uid VARCHAR(36) DEFAULT NULL;';
+        /**
+        * Se evita el mensaje 'ERROR:  cannot ALTER TABLE "apex_objeto" because
+        * it has pending trigger events' de postgres 8.3
+        */
+        $sql = 'SET CONSTRAINTS ALL IMMEDIATE;';
+        $this->elemento->get_db()->ejecutar($sql);
+        $sql = array();
+
+        $sql[] = "ALTER TABLE $schema_logs.apex_sesion_browser ALTER COLUMN ip SET DATA TYPE TEXT;";
+        $sql[] = "ALTER TABLE $schema_logs.apex_solicitud_browser ALTER COLUMN ip SET DATA TYPE TEXT;";
+        $sql[] = "ALTER TABLE $schema_logs.apex_solicitud_consola ALTER COLUMN ip SET DATA TYPE TEXT;";
+        $sql[] = "ALTER TABLE $schema_logs.apex_log_ip_rechazada ALTER COLUMN ip SET DATA TYPE TEXT;";
+        $sql[] = "ALTER TABLE $schema_logs.apex_solicitud_web_service ALTER COLUMN ip SET DATA TYPE TEXT;";
+        $sql[] = 'ALTER TABLE apex_usuario ADD COLUMN uid VARCHAR(36) DEFAULT NULL;';
         $sql[] = 'ALTER TABLE apex_usuario ADD COLUMN p_uid TEXT DEFAULT NULL;';
 
-		$this->elemento->get_db()->ejecutar($sql);
+        $this->elemento->get_db()->ejecutar($sql);
 
-		$sql = 'SET CONSTRAINTS ALL DEFERRED;';
-		$this->elemento->get_db()->ejecutar($sql);
-	}
-
+        $sql = 'SET CONSTRAINTS ALL DEFERRED;';
+        $this->elemento->get_db()->ejecutar($sql);
+    }
 }
-?>
