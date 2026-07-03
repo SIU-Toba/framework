58ede93bfe4d33f7b757b2e7986c4165008b2056
Ricardo Dalinger <rdalinger@siu.edu.ar>
2026-07-03 10:52:48 -0300

    Agrega indice para la tabla apex_msg

---
+++ php/modelo/migraciones/toba_migracion_3_5_0.php
@@ -0,0 +1,22 @@
+<?php
+
+class toba_migracion_3_5_0 extends toba_migracion
+{
+    public function instancia__cambios_estructura()
+    {
+        /**
+        * Se evita el mensaje 'ERROR:  cannot ALTER TABLE "apex_objeto" because
+        * it has pending trigger events' de postgres 8.3
+        */
+        $sql = 'SET CONSTRAINTS ALL IMMEDIATE;';
+        $this->elemento->get_db()->ejecutar($sql);
+        $sql = array();
+
+        $sql[] = 'CREATE INDEX IF NOT EXISTS idx_apex_msg_indice ON apex_msg (trim(indice), proyecto);';
+
+        $this->elemento->get_db()->ejecutar($sql);
+
+        $sql = 'SET CONSTRAINTS ALL DEFERRED;';
+        $this->elemento->get_db()->ejecutar($sql);
+    }
+}
